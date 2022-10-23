c1c4811d93fca5aeed0d68d30260639de6ecbbf9
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2022-10-23 09:57:56 -0400

    Time: 57 ms (22.41%), Space: 13.4 MB (76.15%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -5,21 +5,21 @@ class Solution(object):
         #minimum value of each list and add it to the sum
         #sum = min(list1val)+min(list2val)+...
         #MY APPROACH WILL BE INVALID BC there has to be n people on each city. So 4 people in city A, 4 people in city B. if all the people goes to city A then it is invalid.
+        
         #costs = [[259,770],[448,54],[926,667],[184,139]]
         costs = sorted(costs, key = lambda x: x[0]-x[1], reverse = True)
-        #this prints [[448, 54], [926, 667], [184, 139], [259, 770]]
-        #                394        259           45        -511
-        #this prints [[259,770],[448,54],[926,667],[184,139]]
+        
+        #this prints [[259,770],[448,54],[926,667],[184,139]] reverse helps me to see sort
         #               -511       394     259        45
         n = len(costs) // 2 #this is to have people on both cities
         sumSofar = 0
         for index in range (2*n):
             if index < n:
-                sumSofar += costs[index][1]
+                sumSofar += costs[index][1] #chooses the 
             else:
                 sumSofar += costs[index][0]
         return sumSofar
-        # print(costs)
+        
         
 
         
