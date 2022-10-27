a1a9a513b90a6a9b835a6e8e660c96b898475eb4
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2022-10-27 01:49:38 -0400

    Time: 747 ms (5.36%), Space: 21.5 MB (25.00%) - LeetHub

--- 1236-web-crawler/1236-web-crawler.py
+++ 1236-web-crawler/1236-web-crawler.py
@@ -12,15 +12,59 @@
 class Solution(object):
     def crawl(self, startUrl, htmlParser):
         visited = set()
-        same = startUrl.split('://')[1].split('/')[0]
         ans = []
-        def dfs(node):
-            visited.add(node)
-            children = htmlParser.getUrls(node)
-            ans.append(node)
-            for i in children:
-                if (i not in visited) and (i.split('://')[1].split('/')[0]) == same:
-                    dfs(i)
+        same = startUrl.split('://')[1].split('/')[0]
+        
+        def dfs(url):
+            childrens = htmlParser.getUrls(url)
+
+            if url in visited:
+                return
+            ans.append(url)
+            visited.add(url)
+            for children in childrens:
+                if children.split('://')[1].split('/')[0] != same:
+                    continue
+                dfs(children)
+          #What happens if is not the same
         dfs(startUrl)
         return ans
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #remove the 0th index
+        #visited
+        #make all the connections and discard it
+        #return ans
+        
+        
+#         visited = set()
+#         same = startUrl.split('://')[1].split('/')[0] 
+#         ans = []
+#         def dfs(node):
+#             visited.add(node)
+#             children = htmlParser.getUrls(node)
+#             ans.append(node)
+#             for i in children:
+#                 if (i not in visited) and (i.split('://')[1].split('/')[0]) == same:
+#                     dfs(i)
+#         dfs(startUrl)
+#         return ans
+#         same = "http://example.org/test"
+#         # same = same.rsplit('://')[1]
+#         # additional = same.split('/')[0]
+#         same = same.split('//')[1].split('/')[0]
+        
+#         print(same)
+        # print(additional)
