2fb1ba9eceda2ddd02e6d3bd8a0e18cdd70e847b
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2022-11-03 19:46:38 -0400

    Time: 1035 ms (96.59%), Space: 37.7 MB (76.78%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestPalindrome(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        lookup = Counter(words)
+        p_len = 0
+        mid = 0
+        
+        for word in lookup.keys():
+            if word[0] == word[1]:
+                if lookup[word]%2 == 0:
+                    p_len += lookup[word]
+                else:
+                    p_len += lookup[word]-1
+                    mid = 1
+                    
+            elif word[::-1] in lookup:
+                p_len += min(lookup[word], lookup[word[::-1]])
+        
+        return (p_len + mid) * 2
+        
