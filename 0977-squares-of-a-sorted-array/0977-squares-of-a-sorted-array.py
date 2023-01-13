47fe109e31f4d453605f45efad8db5b7a6686e1c
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-01-13 11:57:20 -0500

    Time: 180 ms (71.92%), Space: 15.4 MB (71.24%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        1. start
+        2. end pointer = len(nnums) - 1:
+        3. whichever square is bigger add it into the result array
+        
+        """
+        # return [i**2 for i in nums]
+        
+        start = 0
+        end = len(nums) - 1
+        result = []
+        while (start <= end):
+            if nums[start]**2 <= nums[end]**2:
+                result.append(nums[end]**2)
+                end -= 1
+            else:
+                result.append(nums[start]**2)
+                start += 1
+        return result[::-1]
+                
+        
