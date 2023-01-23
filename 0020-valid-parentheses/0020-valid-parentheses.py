01a1a1645d5784b2329486d325448e4d2f1818fc
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-01-23 08:16:51 -0500

    Time: 26 ms (41.83%), Space: 13.7 MB (36.81%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        To solve this we traverse through each character of string which consists of sign {}()[] only. When an open parenthesis comes like {, [, (  we push it into the stack. And when the closing parenthesis comes like }, ], )  we compare it from the top of the stack. If the closing parenthesis matches with the top of stack then we pop the character from the stack otherwise we return false.
+        """
+        stack = []
+        
+        for c in s:
+            if c == "(" or c == "{" or c == "[":
+                stack.append(c)
+            elif len(stack): #it means something is in the stack so I can check for closing parentheses
+                el = stack.pop()
+                if el == "(" and c != ")":
+                    return False
+                if el == "{" and c != "}":
+                    return False
+                if el == "[" and c != "]":
+                    return False
+            else:
+                return False #if I am starting with closed parentheses
+        if len(stack):
+            return False
+        else:
+            return True
+                
+            
+                
+        
