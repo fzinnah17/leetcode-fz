b5660544639fa2fb854b328972be94ef4a4f286b
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-01-23 09:12:41 -0500

    Time: 45 ms (12.01%), Space: 13.7 MB (14.30%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,7 +5,8 @@ class Solution(object):
         :rtype: bool
         To solve this we traverse through each character of string which consists of sign {}()[] only. When an open parenthesis comes like {, [, (  we push it into the stack. And when the closing parenthesis comes like }, ], )  we compare it from the top of the stack. If the closing parenthesis matches with the top of stack then we pop the character from the stack otherwise we return false.
         """
-        stack = []
+        from collections import deque
+        stack = deque()
         
         for c in s:
             if c == "(" or c == "{" or c == "[":
