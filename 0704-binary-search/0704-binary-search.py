e78b37f123bd73ba1b0ccbd485ed99c1633dec07
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-01-26 10:22:09 -0500

    Time: 194 ms (64.52%), Space: 14.7 MB (59.49%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        Things needed in a binary tree:
+        1. Sort
+        2. Lower bound and upper bound
+        3. Start iteration:
+            a. FindMiddle element [always do integer division with //]
+            b. if middle < target ,lower bound goes up to the next element of middle
+            c. if middle > target, upper bound goes down to the next element of the middle
+        """
+        
+        left, right = 0, len(nums) - 1
+        
+        
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] == target:
+                return middle #index
+            if nums[middle] < target:
+                left = middle + 1
+            elif nums[middle] > target:
+                right = middle - 1
+        return -1
+        
+            
+        
