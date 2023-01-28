6a17fef6cdeedd3c38cec9a5504e2c3f27661a25
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-01-28 10:54:36 -0500

    Time: 588 ms (32.43%), Space: 28.5 MB (5.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -3,15 +3,25 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
+        A set is an unordered collection of objects that doesn’t allow duplicate elements. Typically, sets are used to quickly test a value for membership in the set, to insert or delete new values from a set, and to compute the union or intersection of two sets.
+
+In a proper set implementation, membership tests are expected to run in fast O(1) time. Union, intersection, difference, and subset operations should take O(n) time on average. 
         """
         
-        hashMap = {}
-        
-        for i in nums:
-            if i in hashMap:
-                hashMap[i] += 1
-                if hashMap[i] > 1:
-                    return True
-            else:
-                hashMap[i] = 1
+        s= set(nums)
+        if len(nums) != len(s):
+            return True
         return False
+        # print(set(nums)) #[1,2,3]
+        # print(len(set(nums))) #3
+        
+#         hashMap = {}
+        
+#         for i in nums:
+#             if i in hashMap:
+#                 hashMap[i] += 1
+#                 if hashMap[i] > 1:
+#                     return True
+#             else:
+#                 hashMap[i] = 1
+#         return False
