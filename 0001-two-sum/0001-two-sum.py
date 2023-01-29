b909b7a46e3ec99362723b7145e24c96139443bf
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-01-29 10:49:21 -0500

    Time: 38 ms (95.00%), Space: 14.3 MB (74.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,27 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
-        start = 0
-        end = len(nums)-1
-        while nums < len(nums):
-        1. sum > target:
-            end -= 1
-        2. sum < target:
-            start += 1
-        3. sum == target:
-            return []   
-            nums = [2,7,11,15]
         """
-        start = 0
-        end = len(nums) - 1
-        while (start <= end):
-            curr = nums[start] + nums[end]
-            if curr == target:
-                return [start, end]
-            end -= 1
-            if start == end:
-                start += 1
-                end = len(nums) - 1
-
+        #[2,3,4,5,7] target = 11
+        # t
+        #{}
+        
+        hashMap = {}
+        
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hashMap:
+                return [hashMap[diff],i]
+            hashMap[nums[i]] = i
+            
+            
+            
         
         
