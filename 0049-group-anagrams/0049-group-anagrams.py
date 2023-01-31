d53b205cb3b62088042e1ff996e451ca4e0b805f
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-01-31 11:56:07 -0500

    Time: 84 ms (71.73%), Space: 17.7 MB (65.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Solution(object):
     def groupAnagrams(self, strs):
         """
@@ -6,18 +7,14 @@ class Solution(object):
         1. How to access strings inside a list?   ------> for loop in (len(strs))
         2. How to access character counts in those strings?  -----> Counter
         ** It is inside a list, so I have to iterate thru the list once to count the number of occurences in each string
+        The Python defaultdict type behaves almost exactly like a regular Python dictionary, but if you try to access or modify a missing key, then defaultdict will automatically create the key and generate a default value for it. This makes defaultdict a valuable option for handling missing keys in dictionaries.
+        for large datasets, it can also be a lot faster and more efficient. So, if speed is a concern for you, then you should consider using a defaultdict instead of a standard dict.
+        
         """
-        l = []
-        d = {}
-        c = {}
-        for i in strs:
-            a = Counter(i)
-            if a in l:
-                for j,k in c.items():
-                    if k == a:
-                        d[j] += [i]
-            else:
-                d[i] = [i]
-                l.append(a)
-                c[i] = a
-        return d.values()
+        anagrams = defaultdict(list) #anything passed in: list, set, int creates {}, (<class 'list'/'set'/'int'> , {})
+        for i in strs: 
+            sortStr = sorted(i) #["a","e","t"]
+            sorted_str = ''.join(sortStr) #"aet"
+            # sorted_str = ''.join(sorted(i)) #https://www.scaler.com/topics/difference-between-sort-and-sorted-in-python/
+            anagrams[sorted_str].append(i) #{'sorted keys': '[actal string values]'}
+        return anagrams.values()
