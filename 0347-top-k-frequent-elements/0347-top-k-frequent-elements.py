62a745441c5f41b301f1b384c1e48d815dd1b244
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-02-01 10:50:22 -0500

    Time: 75 ms (93.60%), Space: 17 MB (48.05%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from collections import Counter
 class Solution(object):
     def topKFrequent(self, nums, k):
@@ -5,15 +6,50 @@ class Solution(object):
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
-        i can check the occurences using counter
-        if counter elements >= k, then return those values
+        Brute Force: Iterate outer for loop k times and inner for loop n times to find the element that is most frequent and unique/not considered in any of the previous iterations
+Time: O(nk)
+Space: O(n) storing elements of nums with its frequency
+Bucket sort is mainly useful when input is uniformly distributed over a range.
         """
-        n = collections.Counter(nums)
-        res =[]
-        for i in n.most_common(k):
-            res.append(i[0])
-        return res
-            
-        #print(n) #Counter({1: 3, 2: 2, 3: 1})
-        #if val> k:
-            #return that val
+        #[4,4,1,3,1,4,2,7]
+        #[    1      2 3 4 5 6 7 8] #O(n) scanning
+        #[[3][2][7]  1 4 x x x x x]  ----> Buckets #O(n) scanning : WE ARE READING FROM RIGHT TO LEFT
+        #[[2,3,7]    1 4 x x x x x] 
+        #       k = 5  <----- RETURNING [4,1,2,3,7],     k = 2 <---- RETURNING [4,1]
+        #       k = 6 IS INVALID BECAUSE WE HAVE ONLY N NUMBERS < K  <------- #TC &SC: O(n)
+        
+        """Steps:
+        1. I am creating a table {key:values}
+            key: i(counts) ---> len(array)
+            values: numbers ---> numbers from the array
+        2. Insert the numbers
+        3. Sort the numbers
+        """    
+        if k == len(nums):
+            return nums
+        #[4,4,1,3,1,4,2,7]
+        #c = Counter(nums) #Counter({ 4: 3,    1: 2,    2: 1,    3: 1,    7: 1 }) unordered
+        d = collections.defaultdict(int) 
+                        #defaultdict(<type 'int'>, {1: 2,  2: 1,  3: 1,  4: 3,  7: 1}) ordered
+        for i in nums:
+            d[i] += 1 #ALTERNATIVE OF Counter
+        f = defaultdict()          # defaultdict({1: [2, 3, 7], 2: [1], 3: [4]})
+        for num, freq in d.items(): # O(n)
+            if freq not in f:
+                f[freq] = [num] 
+            else:
+                f[freq].append(num)
+        res = []
+        for i in range(len(nums),0,-1): # O(n)
+            if i in f:
+                res += f[i]
+        return res[:k] #[4,1]
+    
+        
+        
+        #For testcase, [-1,-1] k = 1 
+        #       d: defaultdict(<type 'int'>, {-1: 2}) 
+        #       f: defaultdict(None, {2: [-1]})
+
+        
+        
