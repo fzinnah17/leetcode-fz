632ba885a0db5110649c1179ffabf5794aae2090
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-02-02 12:00:57 -0500

    Time: 199 ms (55.52%), Space: 20.9 MB (67.54%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,6 +6,23 @@ class Solution(object):
         Infix	  Prefix	Postfix/Suffix
          A+B	   +AB	      AB+
         """
+        #TC: O(n) and SC: O(1)
+        res= [1]*len(nums)
+        preFix = 1
+        postFix = 1
+        
+        for i in range(len(nums)):
+            res[i] *= preFix
+            res[(len(nums)-1)-i] *= postFix
+            #preFix and postFix are updated on each block
+            preFix *= nums[i]
+            postFix *= nums[(len(nums)-1)-i]            
+        return res
+        """
+        For our initial array, we want to find the Prefix Product Array and PostFix Product Array. preFix[i] = preFix[i - 1] * nums[i - 1], for example. (Yes, we multiply with nums[i - 1] rather than nums[i]), and postFix[i] = postFix[i + 1] * nums[i + 1].
+Now, for each index i, our final answer would be result[i] = preFix[i] * postFix[i]. Why? Because preFix[i] * postFix[i] contains the product of each element before and after i, but not the one at index i. (As a result, nums[i] was not included in our preFix and postFix products.)
+
+The Time Complexity would be O(n), but we already have O(n) Auxiliary Space (excluding the final answer array).
         preFix, postFix = [1]*len(nums), [1]*len(nums)
         
         for i in range(1,len(nums)):
@@ -14,7 +31,7 @@ class Solution(object):
             postFix[i] = postFix[i+1]*nums[i+1]   
         for i in range(len(nums)):
             nums[i] = preFix[i]*postFix[i]
-        return nums
+        return nums"""
         
         
         
