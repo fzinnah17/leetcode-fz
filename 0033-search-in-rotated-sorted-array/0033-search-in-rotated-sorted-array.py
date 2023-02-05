1dd07965fbb45c02f8b16fa53d7e2548335bff93
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-02-05 09:30:48 -0500

    Time: 33 ms (39.35%), Space: 14.1 MB (5.67%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            middle = left + ((right - left) // 2) #[4,5,6,7,0,1,2] -> midVal = 7
+            #if midVal = target: return midVal
+            if nums[middle] == target:
+                return middle
+            if nums[left] <= nums[middle]:
+                if nums[left] <= target <= nums[middle]:
+                    right = middle - 1
+                else:
+                    left = middle + 1
+            else:
+                if nums[middle] <= target <= nums[right]:
+                    left = middle + 1
+                else:
+                    right = middle - 1 
+        return -1
+            
