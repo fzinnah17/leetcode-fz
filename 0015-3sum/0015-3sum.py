c5d2bb8e9a8db8d76aaaf9bf82c07694129d4e11
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-02-09 10:05:14 -0500

    Time: 945 ms (59.22%), Space: 16.3 MB (97.34%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,31 +1,33 @@
 class Solution(object):
-    def threeSum(self, nums):        
-        result = []
-        nums.sort() #[-4,-1,-1,0,1]
-        result_set = set()
-        for curr_index, curr_value in enumerate(nums):
-            if curr_index > 0 and curr_value == nums[curr_index - 1]:
-                continue #if the current value is same as previous we disregard the nums
-                #applicable for -1 and -1
-            left, right = curr_index + 1, len(nums)-1
-            while left < right:
-                total = curr_value + nums[left] + nums[right]
-                if total > 0:
-                    right -= 1 #larger sum means move down 
-                elif total < 0:
-                    left += 1 #smaller sum means move up
-                else:
-                    if (curr_value,nums[left],nums[right]) not in result_set:
-                        result.append([curr_value, nums[left], nums[right]])
-                        result_set.add((curr_value,nums[left],nums[right]))
-                    left += 1 #left pointer
-                    right -= 1
-        return result
-    
-                    
-
-                
-                
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        ans = []
+        for i in range(len(nums)): # leave two entry for pointer
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left = i + 1
+            right = len(nums) - 1
             
-        
+            while left < right:
+                
+                newSum = nums[left] + nums[right] + nums[i]
+                if newSum < 0:
+                    left += 1
+                elif newSum > 0:
+                    right -= 1
+                else:
+                    ans.append((nums[i],nums[left],nums[right]))
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+
+                    left += 1
+        return ans
+                    
+                    
+                
+                
         
