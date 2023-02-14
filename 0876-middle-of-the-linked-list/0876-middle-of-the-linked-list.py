d951848f85a5361a822ef3f6bef49a38150fba44
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-02-14 16:01:58 -0500

    Time: 8 ms (97.76%), Space: 13.6 MB (32.79%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -9,13 +9,29 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         https://www.ijrte.org/wp-content/uploads/papers/v8i4/D7296118419.pdf
-        Binary search is inefficient to use in Linked List
+        Binary search is inefficient to use in Linked List, YET we will try to do it
         """
-        slow, fast = head, head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        return slow
+        length = 0
+        start = head 
+        while start:
+            length += 1
+            start = start.next  
+        mid = length//2
+        left = 0
+        while head:
+            if left == mid:
+                return head
+            else:
+                left += 1
+                head = head.next
+        return None
+            
+        
+        # slow, fast = head, head
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+        # return slow
             
             
         
