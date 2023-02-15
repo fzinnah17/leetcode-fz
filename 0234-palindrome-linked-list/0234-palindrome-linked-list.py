e0c535bca0bc9723c157f27f10a333f6a7b7537c
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-02-15 09:51:03 -0500

    Time: 1138 ms (40.38%), Space: 84.9 MB (44.21%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        def reverseList(head):
+            prev = None
+            curr = head
+            while curr:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+            return prev 
+        def midElement(head):
+            fast = head
+            slow = head
+            while fast.next is not None and fast.next.next is not None:
+                fast = fast.next.next
+                slow = slow.next
+            return slow
+            # length = 0
+            # start = head
+            # while start:
+            #     length += 1
+            #     start = start.next
+            # left = 0
+            # middle = length // 2
+            # while head:
+            #     if left == middle:
+            #         return head
+            #     else:
+            #         left += 1
+            #         head = head.next
+            # return None
+        
+        firstHalf = midElement(head)
+        secondHalf = reverseList(firstHalf.next)
+        result = True
+        firstPointer = head
+        secondPointer = secondHalf
+        while result and secondPointer != None :
+            if firstPointer.val != secondPointer.val:
+                result = False
+            firstPointer = firstPointer.next
+            secondPointer = secondPointer.next
+        firstHalf.next = reverseList(secondHalf)
+        return result
+        
+            
+        
