7fc43bd5770f1b68378467563f4f78dd6864e802
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-03-13 11:16:45 -0400

    Time: 1321 ms (48.86%), Space: 60.1 MB (11.51%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -8,19 +8,19 @@ class Solution(object):
         4. insert the first element in the list
         5. traverse the input
             a. check if the current' start time > previous' end time
-                    i. If so, then add it in the list
-        6. return the len(intervals) - len(nonoverLapping)
+                    i. If so, then output += 1
+        6. return the output
         TC: O(NLOGN) SC: O(N)
         """
         output = 0
         if len(intervals) == 1:
             return output
         intervals.sort() #[[1,2],[1,3],[2,3],[3,4]]
-        nonoverLapping = []
-        nonoverLapping.append(intervals[0])
+        lst = []
+        lst.append(intervals[0])
         
         for i in range(1,len(intervals)):
-            prev = nonoverLapping[-1]
+            prev = lst[-1]
             curr = intervals[i]
             if curr[0] >= prev[1]: #nonoverlapping
                 prev[1] = curr[1]
