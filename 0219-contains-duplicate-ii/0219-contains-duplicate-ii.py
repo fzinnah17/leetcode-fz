ce5985b81e7c6ff190c2e587e40a75cb8a996bca
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-04-27 10:49:34 -0400

    Time: 541 ms (12.29%), Space: 24.7 MB (41.49%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -10,4 +10,14 @@ class Solution(object):
                 else:
                     hashMap[nums[i]] = i
         return False
-                
+    """
+    Pseducode:
+    1. use a hashMap to check if the number already exists
+    2. traverse the array
+        a. if not in the hashmap, then add it
+        b. if in the hashmap:
+            i. check if the current index position - previous index position <= k:
+                        return True
+            ii. if NOT > k:
+                        update the previous index position with the current index position
+    3. return False if after traversing we see that there are no indices that meet the conditions given"""
