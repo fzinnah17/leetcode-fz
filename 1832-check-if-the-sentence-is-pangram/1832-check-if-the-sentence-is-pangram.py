f38497fbbf5e3f569f13c00230f56657251304a7
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-05-02 12:12:03 -0400

    Time: 18 ms (61.97%), Space: 13.5 MB (34.00%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -4,8 +4,8 @@ class Solution(object):
 1. create a hashMap
 2. add the keys and values to the hashMap by iterating the input
 3. iterate over the lowercase letters(or whatever the question requires)
-    a. if that specific character is in the hashMap, then return True
-    b. Or return False
+    a. if that specific character is not in the hashMap, then return False
+    b. Or return True if all the characters are present 
 Yes we can compare a list of characters with a dictionary containing key-value pairs. One way to check if all the characters in the list are present in the dictionary is to iterate over the list of characters and check if each character exists as a key in the dictionary. 
         """
         hashMap = {}
