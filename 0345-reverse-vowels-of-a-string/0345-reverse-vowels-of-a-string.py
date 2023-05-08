f5778a119abc9bbbcb4a8f203fb5939072203c31
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-05-08 07:44:44 -0400

    Time: 71 ms (44.62%), Space: 15.3 MB (82.50%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -3,15 +3,17 @@ class Solution(object):
         """
         Pseudocode:
         1. Create own SET of vowels to check if the string characters are vowel or not
-        2. Have two pointers
+        2. Create a list from the input string as the string positions can't be changed
+        3. Have two pointers
             a. P1 to start in the beginning
             b. P2 to start from the end
-        3. iterate through the string using while loop until the two pointers meet in the middle of the string
-        4. As iterating,
+        4. iterate through the string using while loop until the two pointers meet in the middle of the string
+        5. As iterating,
             we will check:
-                if P1 = vowel and P2 = vowel, then we can swap those, but in Python strings are immutable/unchangeble/fixed/cant be changed, meaning that you cannot change a string in-place. Thus, the swapping s[left],s[right] = s[right],s[left] will not work.
-                if P1 = vowel and P2 != vowel, then we can increment P1 and decrememnt P2 till P2 cross P1
-        5. At the end we can return the string
+                if P1 = vowel and P2 = vowel, then we can swap those, but in Python strings are immutable/unchangeble/fixed/cant be changed, meaning that you cannot change a string in-place. Thus, the swapping s[left],s[right] = s[right],s[left] will not work. So this method is done only after creating a list from the string
+                if P1 != vowel and P2 = vowel, then we can increment P1 
+                if P1 = vowel and P2 != vowel, then we can decrememnt P2             
+        6. At the end we can return the new string after joining it
         """
         vowels = ["a","e","i","o","u","A","E","I","O","U"]
         lst = list(s)
@@ -28,5 +30,6 @@ class Solution(object):
                 left += 1
         output = "".join(lst)
         return output
+    #TC: O(n) S: O(n)
             
         
