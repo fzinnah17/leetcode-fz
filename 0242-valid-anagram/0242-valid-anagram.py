c24784cc3649e01151a9ded7464cee2584bde072
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-05-15 07:12:51 -0400

    Time: 47 ms (44.94%), Space: 14 MB (68.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,26 +1,13 @@
 class Solution(object):
-    from collections import Counter
     def isAnagram(self, s, t):
         """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        A Counter is a dict subclass for counting hashable objects. It is a collection where elements are stored as dictionary keys and their counts are stored as dictionary values.
+        Counter is a frequency table. It is an object. 
+        Key       :     Values
+        char/num  :     Frequency
         """
-        """use counter for both and check if it is equal. If so, return True"""
-        str1 = collections.Counter(s)
-        str2 = collections.Counter(t)
-        
-        if str1 == str2:
+        s = collections.Counter(s)
+        t = collections.Counter(t)
+        if s == t:
             return True
         return False
-        
-        # print(str1)
-        # print(str2)
-#         str1 = Counter({u'a': 3, u'r': 1, u'm': 1, u'g': 1, u'n': 1}) 
-#         str2 = Counter({u'a': 3, u'r': 1, u'm': 1, u'g': 1, u'n': 1})
-        #TC & SC: O(1) 
-        
-        
-        
         
