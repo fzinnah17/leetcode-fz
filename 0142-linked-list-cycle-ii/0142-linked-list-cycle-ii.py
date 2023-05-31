e43c2cc99cb39871c61c34ec5ab3ba054ae71f37
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-05-31 08:32:56 -0400

    Time: 46 ms (24.41%), Space: 19.6 MB (72.24%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -6,35 +6,48 @@
 
 class Solution(object):
     def detectCycle(self, head):
-        slow, fast = head, head
-        cycleLength = 0
+        """
+        1. Identify the problem: problem statement indicating the presence of a cycle
+        2. Handle edge cases: linked list or array that is empty or a structure that doesn't have any cycles.
+        if head is None:
+            return None
+        3. Initialize the pointers:
+                a. fast = head,
+                b. slow = head
+        4. Use while loops with the given problem condition:
+            a. To move the pointers until you reach the end of the data structure or when you find a cycle.
+        while fast and fast.next:
+        5. Choose a movement strategy:
+            a. Tortoise and Hare: The fast pointer (the hare) moves faster than the slow pointer (the turtle).
+        fast = fast.next.next
+        slow = slow.next
+        6. Find cycles or repeating parts: This could happen when the fast pointer catches up to the slow pointer or when they both end up at the same element.
+        if fast == slow:
+        7. Time and space complexity: time complexity of O(N) and a space complexity of O(1)   
+                """
+        slow = fast = head
+
+        if not head or not head.next:
+            return None
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             if slow == fast:
-                current = slow
-                while True:
-                    current = current.next
-                    cycleLength += 1
-                    if current == slow:
-                        break
-                break #mandatory to break the loop
-        if cycleLength > 0 and slow == fast:
-            slow = head
-            while fast != slow:
-                slow = slow.next
-                fast = fast.next
-            return fast
+                break
         else:
-            return 
-        #TC: finding the cycle in a LinkedList with ‘N’ nodes: O(n)
-        #.   finding the length of the cycle: O(n)
-        #    find the start of the cycle: O(n) 
-        #Therefore, the overall time complexity of our algorithm will be O(N).
-        
-        #SC: O(1)
-                    
+            return None
             
-                        
+        pointer = head
+        while pointer != fast:
+            pointer = pointer.next
+            fast = fast.next
+        return pointer
+                
+                
+                
+                
+            
+            
+        
         
         
