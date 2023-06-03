54435faa5aa978a45a1f347cc4ea6fa6ec849efa
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-06-03 07:17:57 -0400

    Time: 15 ms (75.95%), Space: 13.3 MB (94.78%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -6,35 +6,16 @@
 class Solution(object):
     def middleNode(self, head):
         """
-        :type head: ListNode
-        :rtype: ListNode
-        https://www.ijrte.org/wp-content/uploads/papers/v8i4/D7296118419.pdf
-        Binary search is inefficient to use in Linked List, YET we will try to do it
+        I can use the tortoise and hare method here.
+        The slow pointer goes one step
+        The fast pointer goes two steps
+        while checking the 'if' conditions:
+            if fast.next.next doesn't exist, then return slow(middle node)
+        TC: O(n) for n nodes. SC: O(1) no space used
         """
-        length = 0
-        start = head 
-        while start:
-            length += 1
-            start = start.next  
-        mid = length//2
-        left = 0
-        while head:
-            if left == mid:
-                return head
-            else:
-                left += 1
-                head = head.next
-        return None #TC: O(n) SC: O(1)
-            
-        
-        # slow, fast = head, head
-        # while fast and fast.next:
-        #     slow = slow.next
-        #     fast = fast.next.next
-        # return slow
-        #TC: O(n) SC: O(1)
-        
-            
-            
-        
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow #if both the fast and fast.next doesn't exist
         
