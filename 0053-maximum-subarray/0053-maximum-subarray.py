42281107f3fd291b30a5964dfe8281df4f0a6c59
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-06-04 10:59:36 -0400

    Time: 610 ms (48.91%), Space: 26.3 MB (78.32%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,30 +1,34 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        '''My approach: if at any point sum returns - number we can just disregard that and keep it 0 to add later. If the value is + then we can add it our sum and update maximum sum.'''
-        maxSofar = nums[0]
-        currSum = 0
-        for i in nums:
-            if currSum < 0:
-                currSum = 0
-            currSum += i
-            maxSofar = max(maxSofar,currSum)
-        return maxSofar
-
-        # for i in range(1,len(nums)):
-        #     nums[i] = max(nums[i],nums[i]+nums[i-1])
-        # return max(nums)
-
         """
-        Using Kadnae's Algorithm
+        1. Handle edge cases/constraints
+        2. Initialize variables: sum
+        3. Calculate the initial result: Of the first window and check if it is > 0 or < 0
+        4. Slide the window: Iterate through the array or linked list using a loop, adjusting the sliding window by moving the start and end indices as needed
+        5. Update the result: The result will be updated based on the current window.
+                I. Determine the sliding window size: We can start by making the window size expand one at a time.
+                    a. if the addition of the window size is < 0, then we can remove the first number of the window
+                    b. if the addition of the window size is > 0 or > prevSum, then keep expanding the window
+        6. we will return the sum using max() function
         """
-        # maxCurrindex = nums[0]
-        # maxSofar = nums[0]
-        
-        # for curr_index in nums[1:]:
-        #     maxCurrindex = max(maxCurrindex+curr_index, curr_index)
-        #     maxSofar = max(maxSofar, maxCurrindex)
-        # return maxSofar
-        
+        ans = float("-inf")
+        runSum = 0
+        for i in range(len(nums)):
+            if runSum < 0:
+                runSum = nums[i]
+            else:
+                runSum += nums[i]
+            ans = max(ans, runSum)
+        return ans
+    #TC: O(n) SC: O(1)
+            
+    """Lesson:
+the equal sign (=) is used for assignment, while the double equal sign (==) is used for comparison.
+
+When you write runSum = nums[i], you are assigning the value of nums[i] to the variable runSum. This means that runSum will now hold the same value as nums[i].
+
+On the other hand, when you write runSum == nums[i], you are checking if the value of runSum is equal to the value of nums[i]. This expression will return either True or False, depending on whether the values are equal or not.
+"""
         
         
         
