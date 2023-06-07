7cccfc684814fda42771f85942168dee33e380ab
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-06-07 18:35:38 -0400

    Time: 52 ms (67.04%), Space: 13.5 MB (51.51%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,50 +6,39 @@
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        1. Have two pointers
-        2. if l1 and l2:
-            Two pointers sum < 10   -----> add it in the linked list
-            Two pointers sum > 10   -----> add the modulo of the addition (%) in the list
-                            take the quotient of the number and add it with the next addition
-        3. Edge case:
-                    a. l1 length > l2 length then put the remaining l1 length in the output
-                    b. l2 length > l1 length then put the remaining l2 length in the output
-        TC: O(m+n) length of the nodes   SC: O(1) for no space used, BUT if we count the output as memory, then it is O(max(m,n)).
+        Start with an empty result list: to store the result of the addition.
+        Traverse the linked lists: Keeping track of the current node in each list
+        add the corresponding digits and the previous carry if there is any
+        Handling the carry
+        How do I consider the scenario where one linked list is longer than the other.
         """
-        #.next after means change position
         carry = 0
         dummy = prev = ListNode(None)
         
         while l1 or l2 or carry:
-            # if l1:
-            #     digit1 = l1.val
-            # else:
-            #     digit1 = 0
-            # if l2:
-            #     digit2 = l2.val
-            # else:
-            #     digit2 = 0
-            digit1 = l1.val if l1 else 0
-            digit2 = l2.val if l2 else 0
-            if l1 and l2:
-                sumNodes = digit1 + digit2 + carry
-                l1 = l1.next
-                l2 = l2.next
-            elif l1:
-                sumNodes = digit1 + carry
-                l1 = l1.next if l1 else None
+            if l1:
+                val1 = l1.val
             else:
-                sumNodes = digit2 + carry
-                l2 = l2.next if l2 else None
-            carry = sumNodes // 10 #quotient
-            sumNodes = sumNodes % 10 #modulo
-            prev.next = ListNode(sumNodes)
+                val1 = 0
+            if l2:
+                val2 = l2.val
+            else:
+                val2 = 0
+            total = val1 + val2 + carry
+            modNum = total % 10
+            carry = total // 10
+            
+            prev.next = ListNode(modNum)
+            
             prev = prev.next
             
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
         return dummy.next
+    #TC: O(m + n), m is the size of l1 and n is the size of l2
+    #SC: O(1) for no space used
                 
-                
-            
+        
+        
