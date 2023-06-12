acd5e586f6e78968d9e95530f12a697173cfcaaf
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-06-12 11:14:54 -0400

    Time: 16 ms (94.53%), Space: 13.4 MB (40.29%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -6,38 +6,28 @@
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        Brute force Approach: We can create a new empty list. Then, we can iterate over the first list and add it into the new empty list. Afterwards, we can follow the same approach with the second list. As the list is filled with both given list nodes, we can sort the new list. After sorting, we have to assign the pointers to create a new LinkedList. It means we are reconstructing a new linkedlist maintaining the order of the new array.
-        #TC: O(L+M(Nlogn)) = L+M is the nodes from both lists. SC: O(N)
-        This is inefficient because of three factors:
-                1. We do not take use of the benefits of a linkedlist. LinkedLists are pretty flexible due to their pointers; if we know the references, we can move them in the array in constant time.
-                2. We do not make advantage of the problem requirement. The requirement states clearly that L1 and L2 must be sorted independently. Sorting on a new array therefore ignores the fact that these lists are already sorted.
-                3. In our case, we make use of additional space. A new array and linkedlist are formed to store the values from L1 and L2.
+        TC: O(N) SC: O(N)
         """
+        temp = ListNode(0)
+        current = temp #Initialize Pointers and Variables
         
-        head = ListNode(None)
-        start = head #I can not lose the reference of my root so need to hold it before everything
-        while list1 and list2:
-            if list1.val < list2.val:
-                head.next = list1 #head is being initialized to the next value so .next is before
-                list1 = list1.next #assigning so .next is coming later
+        if not list1 and not list2:
+            return None #Handle Edge Cases
+        
+        list1Ptr = list1 #Initialize Pointers and Variables
+        list2Ptr = list2 
+        
+        while list1Ptr and list2Ptr: #Traverse the Linked List
+            if list1Ptr.val < list2Ptr.val: #Manipulate Pointers and Nodes
+                current.next = list1Ptr #Keep Track of Relevant Information(current connects to other list values and the position of it moves according to the conditional statements)
+                list1Ptr = list1Ptr.next
             else:
-                head.next = list2
-                list2 = list2.next
-            head = head.next
-        if list1 != None and list2 == None:
-            head.next = list1
-        elif list1 == None and list2 != None:
-            head.next = list2
-        elif list1 == None and list2 == None:
-            return None
-        return start.next
-    #TC: O(n) SC: O(1)
-    
-    
-    
-    
-    
-            
+                current.next = list2Ptr
+                list2Ptr = list2Ptr.next #Implement the Solution Logic continues until one of the input lists is exhausted
+            current = current.next
+        if list1Ptr: #still has remaining nodes after the loop
+            current.next = list1Ptr
+        if list2Ptr:
+            current.next = list2Ptr
+        return temp.next
+                   
