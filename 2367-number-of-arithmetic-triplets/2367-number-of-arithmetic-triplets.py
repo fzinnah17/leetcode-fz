fa86c95b8c94a2f90191d5ca9cf5f0cb93b673e5
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-06-20 13:39:49 -0400

    Time: 20 ms (92.17%), Space: 13.2 MB (97.86%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -1,12 +1,26 @@
 class Solution(object):
     def arithmeticTriplets(self, nums, diff):
         """
+        Optmized: TC: O(n) SC: O(n)
         """
+        count = 0
+        hashSet = set(nums)
+        #(1,4,7) diff = 3  4 - 3 = 1 and 4 + 3 = 7 both in the set
+        #(4,7,10) diff = 3 7 - 3 = 4 and 7 + 3 = 10 both in the set
+        for i in range(len(nums)):
+            subNum = nums[i] - diff
+            addNum = nums[i] + diff
+            if subNum in hashSet and addNum in hashSet:
+                count += 1
+        return count
+            
+            
+"""Brute Force: O(n^3)
         count = 0
         for i in range(len(nums)):
             for j in range(1,len(nums)):
                 for k in range(2,len(nums)):
                     if nums[k] - nums[j] == diff and nums[j] - nums[i] == diff:
                         count += 1
-        return count
+        return count"""
         
