be59c950640ea0421a7b985237f22718225f2d1d
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-06-23 10:12:18 -0400

    Time: 149 ms (14.37%), Space: 23.3 MB (5.00%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,34 @@
-from collections import defaultdict
 class Solution(object):
     def groupAnagrams(self, strs):
         """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        1. How to access strings inside a list?   ------> for loop in (len(strs))
-        2. How to access character counts in those strings?  -----> Counter
-        ** It is inside a list, so I have to iterate thru the list once to count the number of occurences in each string
-        The Python defaultdict type behaves almost exactly like a regular Python dictionary, but if you try to access or modify a missing key, then defaultdict will automatically create the key and generate a default value for it. This makes defaultdict a valuable option for handling missing keys in dictionaries.
-        for large datasets, it can also be a lot faster and more efficient. So, if speed is a concern for you, then you should consider using a defaultdict instead of a standard dict.
-        
+        Pseudocode:
+        1. variables: result = [] needs to be resetted to empty after the anagrams are grouped but how? ,                      output = [], pointer
+        2. edge case: if not array: return empty array
+                      if len(array) == 1: return the array
+        3. Traverse through each string of the array
+            if Counter(i value) != Counter(pointer value):
+                pointer += 1
+            else:
+                add it to the result with result.append()
+                pointer += 1
+            pointer += 1 (as it will be moving any way)
+            the loop will keep on going till the pointer reaches the end of the string    
+        TC: O(n) SC: O(n)
         """
-        anagrams = defaultdict(list) #anything passed in: list, set, int creates {}, (<class 'list'/'set'/'int'> , {})
-        for i in strs: 
-            sortStr = sorted(i) #["a","e","t"]
-            sorted_str = ''.join(sortStr) #"aet"
-            # sorted_str = ''.join(sorted(i)) #https://www.scaler.com/topics/difference-between-sort-and-sorted-in-python/
-            anagrams[sorted_str].append(i) #{'sorted keys': '[actal string values]'}
-        return anagrams.values()
+        output = []
+        result = {}
+
+        if not strs:
+            return []
+        if len(strs) == 1:
+            return [strs]
+        for i in range(len(strs)):
+            count = Counter(strs[i])
+            countTuple = tuple(sorted(count.items()))
+            if countTuple not in result:
+                result[countTuple] = [strs[i]]
+            else:
+                result[countTuple].append(strs[i])
+        for group in result.values():
+            output.append(group)
+        return output
