f9da8b1a872566f98916960cbc79a5534cc8545d
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-07-05 21:20:24 -0400

    Time: 110 ms (90.71%), Space: 18.1 MB (62.33%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,21 +1,25 @@
 class Solution(object):
     def merge(self, intervals):
         """
-        TC: O(nlogn) because we are sorting
-        SC: O(N) for using priority queue
+        Pseudocode:
+        Edge case: the intervals length <= 2 means just return the intervals itself.
+        1. sort the array based on the start values
+        2. variables
+        3. traverse the array:
+            a. current array start value > previous array end value:
+                    merge those
+            b. else: current array start value < previous array end value:
+                    append those in the output array
+        TC: O(nlogn) SC: O(n)
         """
-        intervals.sort()
-        prQueue = []
-        prQueue.append(intervals[0])
-
+        if len(intervals) <= 1:
+            return intervals
+        intervals.sort(key = lambda x : x[0]) #sort based on the starting value
+        output = []
+        output = [intervals[0]]
         for i in range(1,len(intervals)):
-            curr = intervals[i]
-            prev = prQueue[-1]
-            if curr[0] <= prev[1]:
-                prQueue[-1][1] = max(curr[1],prev[1])
+            if intervals[i][0] <= output[-1][1]:
+                output[-1][1] = max(intervals[i][1],output[-1][1])
             else:
-                prQueue.append(curr)
-        return prQueue
-                
-            
-        
+                output.append(intervals[i])
+        return output
