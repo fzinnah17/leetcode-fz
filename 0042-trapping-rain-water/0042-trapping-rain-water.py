3715dcc6bfdd080d8223d5e0eafc8f9fa34ab53c
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-07-09 12:04:16 -0400

    Time: 86 ms (93.08%), Space: 14.8 MB (98.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,22 +1,39 @@
 class Solution(object):
     def trap(self, height):
         """
-        :type height: List[int]
-        :rtype: int
+        Pseudocode:
+        1. variables: 
+            a. left = 0, right = len(array) - 1 as it reference both sides of the array
+            b. water = 0
+            c. leftMax = array[left] maximum bar heights encountered so far from the left side
+            d. rightMax = array[right] maximum bar heights encountered so far from the left side
+        2. Traverse the array from left to right using while loop:
+            while left < right:
+            The idea is to compare both leftMax and rightMax to see where the water is trapped
+            a. if rightMax > leftMax:
+                the water should depend on the left bar as the left bar is small
+                i. left += 1
+                ii. update the rightMax using the max function. max(maxLeft,array[left])
+                iii. calculate the water: water += maxLeft - array[left]
+            b. else: rightMax < leftMax:
+                the water should depend on the right bar as the right bar is small
+                i. right -= 1
+                ii. update the rightMax using the max function. max(maxRight,array[right])
+                iii. calculate the water: water += maxRight - array[right]
+        3. Return the water variable
+        TC: O(n) SC: O(1)
         """
-        left = 0
-        right = len(height) - 1
-        maxLeft = height[left]
-        maxRight = height[right]
-        water = 0
-
+        left, right = 0, len(height) - 1
+        water, leftMax, rightMax = 0, height[left], height[right]
+        
         while left < right:
-            if maxLeft < maxRight:
+            if rightMax > leftMax:
                 left += 1
-                maxLeft = max(maxLeft, height[left])
-                water += maxLeft - height[left]
+                leftMax = max(leftMax, height[left])
+                water += leftMax - height[left]
             else:
                 right -= 1
-                maxRight = max(maxRight, height[right])
-                water += maxRight - height[right]
+                rightMax = max(rightMax, height[right])
+                water += rightMax - height[right]
         return water
+        
