99d8b1b4de35c7a83c8810a442398ec549914599
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-07-20 12:24:31 -0400

    Time: 625 ms (46.37%), Space: 14.1 MB (23.66%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,44 +1,37 @@
 class Solution:
     def maximumDetonation(self, bombs):
-        """"""
-        # Depth-First Search (DFS) function to traverse the graph
-        def dfs(graph, visited, count, i):
-            # Mark the current node as visited
-            visited[i] = True
-            # Increment the count of visited nodes
-            count[0] += 1
-            # Explore neighbors of the current node
-            for j in graph[i]:
-                # If the neighbor is not visited, recursively call the DFS function
-                if not visited[j]:
-                    dfs(graph, visited, count, j)
-        
-        # Get the number of bombs
-        n = len(bombs)
-        # Create an empty graph to represent the relationships between bombs
-        graph = [[] for _ in range(n)]
-        
-        # Build the graph by checking the distance between each pair of bombs
-        for i in range(n):
-            x1, y1, r1 = bombs[i]
-            for j in range(n):
+        def is_connected(a,b):
+            x1, y1, r1 = bombs[a]
+            x2, y2, r2 = bombs[b]
+            dist = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+            return dist <= r1
+
+
+        conn = collections.defaultdict(list)
+        for i in range(len(bombs)):
+            for j in range(len(bombs)):
                 if i != j:
-                    x, y = bombs[j][0], bombs[j][1]
-                    # Calculate the squared distance between two bombs using the coordinates
-                    distance_sq = (x - x1) ** 2 + (y - y1) ** 2
-                    # If the distance is within the range of the bomb's detonation radius, add an edge in the graph
-                    if distance_sq <= r1 ** 2:
-                        graph[i].append(j)
-        
-        # Initialize the maximum detonation count as negative infinity
-        max_detonation = float('-inf')
-        # Traverse each bomb as a starting point for DFS to find the maximum detonation count
-        for i in range(n):
-            count = [0]  # Initialize the count of visited nodes
-            visited = [False] * n  # Create an array to keep track of visited nodes
-            dfs(graph, visited, count, i)  # Perform DFS starting from the current bomb
-            # Update the maximum detonation count if necessary
-            max_detonation = max(max_detonation, count[0])
-        
-        # Return the maximum detonation count
-        return max_detonation
+                    if is_connected(i,j):
+                        conn[i].append(j)
+        def dfs(node):
+
+            if node in visited:
+                return 0
+
+            visited.add(node)
+            
+            ans = 1
+
+            if node in conn:
+                for child in conn[node]:
+                    if child in visited:
+                        continue
+                    ans += dfs(child)
+            
+            return ans 
+
+        maxCount = 1
+        for node in conn:
+            visited = set()
+            maxCount = max(maxCount, dfs(node))
+        return maxCount
