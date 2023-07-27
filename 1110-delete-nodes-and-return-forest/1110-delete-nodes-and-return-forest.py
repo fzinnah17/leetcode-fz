26c758d21287046dba6e150aeefdf3d92b4a3ea3
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-07-27 11:12:00 -0400

    Time: 49 ms (74.59%), Space: 13.7 MB (97.30%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
@@ -7,33 +7,39 @@
 class Solution(object):
     def delNodes(self, root, to_delete):
         """
-        TC: O(n) because we are traversing the entire binary tree once using a recursive depth-first search approach. Each node in the tree is visited once, and the time complexity depends on the number of nodes in the tree, which is n.
-        SC: O(n)
-        """
-        output = []
-        deleteSet = set(to_delete)
-        def helper(node, stayasParent):
+        Pseudocode:
+        1. Variables: Output = [], to_delete = set([to_delete])
+        2. Helper function(currNode, to_delete, stayasParent):
+            Edge case:
             if not node:
                 return None
-            if node.val in deleteSet:
-                node.left = helper(node.left,stayasParent = False)
-                node.right = helper(node.right,stayasParent = False)
-                return None
+            if node.val not in to_delete set:
+                Helper function on left tree(currNode.left, to_delete, stayasParent = True)
+                Helper function on right tree(currNode.right, to_delete, stayasParent = True)
+                as long as stayasParent = True as there's nothing to remove:
+                    add the values in the output list as it will not be removed
+                return the node
+            
             else:
-                node.left = helper(node.left,stayasParent = True)
-                node.right = helper(node.right,stayasParent = True)
-            if not stayasParent: #stayasParent = True
-                output.append(node)
-            return node
-        helper(root, stayasParent = False)
+                remove the node.val by referncing to None
+        3. Helper function(root, to_delete, stayasParent = False)
+        4. return output
+        TC: O(n) SC: O(n)
+        """
+        output = []
+        to_delete = set(to_delete)
+        def helper(currNode, to_delete, stayasParent):
+            if not currNode:
+                return None
+            if currNode.val not in to_delete:
+                currNode.left = helper(currNode.left, to_delete, stayasParent = True)
+                currNode.right = helper(currNode.right, to_delete, stayasParent = True)
+                if not stayasParent:
+                    output.append(currNode) #adding the values
+                return currNode
+            else:
+                currNode.left = helper(currNode.left, to_delete, stayasParent = False)
+                currNode.right = helper(currNode.right, to_delete, stayasParent = False)
+                return None        
+        helper(root, to_delete, stayasParent = False)
         return output
-                
-        
-        
-                
-                
-                
-                
-                
-                
-        
