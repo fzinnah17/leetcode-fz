e1179c4ed9b6e3f631d0217b79881dc68aaa8a19
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-07-28 16:44:19 -0400

    Time: 1024 ms (56.60%), Space: 154.7 MB (42.45%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -5,34 +5,47 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-        def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-            def dfs(node):  #get LCA
-                if not node: return None
-                if node.val in [startValue, destValue]:
-                    return node
-                left = dfs(node.left)
-                right = dfs(node.right)
-                if left and right:
-                    return node
-                return left or right
+    def getDirections(self, root, startVal, destVal):
+        """
+       Just like in the forest, in a binary tree, there are multiple paths connecting different nodes. We need to determine the point where the paths from the start node and the destination node begin to diverge. This point of divergence is exactly what we call the Lowest Common Ancestor (LCA) in our binary tree.
 
-            root = dfs(root)
-            temp, path = [], []
-
-            def getPath(src,dst):
-                if not src:
-                    return
-                if src.val == dst:
-                    temp.append(path[:])
-                    return
-                path.append("L")
-                getPath(src.left,dst)
-                path.pop()
-                path.append("R")
-                getPath(src.right,dst)
-                path.pop()
-
-            getPath(root,startValue)
-            getPath(root,destValue)
-
-            return len(temp[0]) * "U" + "".join(temp[1])
+The LCA helps us to efficiently split the problem into two manageable parts. We can separately find the path from the start node to the LCA and the path from the LCA to the destination node. By doing this, we simplify our problem-solving approach and can analyze the two paths step by step.
+By finding the LCA, we can narrow down our focus, making our solution more organized and efficient. Once we have the two paths, it becomes much easier to analyze them and extract the directions required to navigate from the start node to the destination node.
+"""
+        def dfs(node):
+            if not node:
+                return None
+            if node.val in [startVal,destVal]:
+                return node
+            left = dfs(node.left)
+            right = dfs(node.right)
+            if left and right:
+                return node
+            return left or right
+        found = dfs(root)
+        
+        def helper(curr, lookupNode):
+            if not curr:
+                return ""
+            if curr.val == lookupNode:
+                return ""
+            if curr.left and curr.left.val == lookupNode:
+                return "L"
+            if curr.right and curr.right.val == lookupNode:
+                return "R"
+            leftPath = helper(curr.left, lookupNode)
+            rightPath = helper(curr.right, lookupNode)
+            
+            if leftPath:
+                return "L" + leftPath
+            
+            if rightPath:
+                return "R" + rightPath
+            
+            return ""
+            
+        startPath = helper(found, startVal)
+        endPath = helper(found, destVal)
+        
+        return len(startPath) * "U" + endPath
+        
