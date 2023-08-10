b3ba2f38c714819108533346fea101d5a322ab05
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-08-09 21:32:20 -0400

    Time: 22 ms (66.56%), Space: 13.8 MB (11.88%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,25 +1,18 @@
 class Solution(object):
     def wordBreak(self, s, wordDict):
-        """
         
-        1. table = {}
-        leetcode
-        c
-           i
-        """
         memoTable = {}
         def helper(curr):
             if curr == len(s):
                 return True
             if curr in memoTable:
                 return memoTable[curr]
-            for word in wordDict:
-                #leet code
-                #        c
-                if s[curr:curr+len(word)] == word and helper(curr+len(word)):
+            for i in range(curr + 1, len(s) + 1):
+                if s[curr:i] in wordDict and helper(i):
                     memoTable[curr] = True
-                    return memoTable[curr]
+                    return True
             memoTable[curr] = False
             return False
+      
         return helper(0)
         
