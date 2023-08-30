d1341cc53900bb6770005609425f902699881c15
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-08-30 14:53:03 -0400

    Time: 430 ms (71.87%), Space: 29.2 MB (29.92%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def containsDuplicate(self, nums):
         """
-        :type nums: List[int]
-        :rtype: bool
+        if numbers are repeated -> return True
+        if numbers are not repeated -> return False
+        TC: O(1) SC: O(n)
         """
-        t = set(nums)
-        if len(nums) != len(t):
+        if len(set(nums)) != len(nums):
             return True
         return False
+        
         
