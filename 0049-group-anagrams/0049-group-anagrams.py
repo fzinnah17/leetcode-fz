dc04d65a71111e4755c89d4444932bf9df5e4fa4
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-09-01 17:35:29 -0400

    Time: 67 ms (86.38%), Space: 17.3 MB (72.66%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,34 +1,9 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        """
-        Pseudocode:
-        1. variables: result = [] needs to be resetted to empty after the anagrams are grouped but how? ,                      output = [], pointer
-        2. edge case: if not array: return empty array
-                      if len(array) == 1: return the array
-        3. Traverse through each string of the array
-            if Counter(i value) != Counter(pointer value):
-                pointer += 1
-            else:
-                add it to the result with result.append()
-                pointer += 1
-            pointer += 1 (as it will be moving any way)
-            the loop will keep on going till the pointer reaches the end of the string    
-        TC: O(n) SC: O(n)
-        """
-        output = []
-        result = {}
-
-        if not strs:
-            return []
-        if len(strs) == 1:
-            return [strs]
-        for i in range(len(strs)):
-            count = Counter(strs[i])
-            countTuple = tuple(sorted(count.items()))
-            if countTuple not in result:
-                result[countTuple] = [strs[i]]
-            else:
-                result[countTuple].append(strs[i])
-        for group in result.values():
-            output.append(group)
-        return output
+        anagrams = defaultdict(list) #anything passed in: list, set, int creates {}, (<class 'list'/'set'/'int'> , {})
+        for i in strs: 
+            sortStr = sorted(i) #["a","e","t"]
+            sorted_str = ''.join(sortStr) #"aet"
+            # sorted_str = ''.join(sorted(i)) #https://www.scaler.com/topics/difference-between-sort-and-sorted-in-python/
+            anagrams[sorted_str].append(i) #{'sorted keys': '[actal string values]'}
+        return anagrams.values()
