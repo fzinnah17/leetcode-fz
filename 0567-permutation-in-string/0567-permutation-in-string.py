cd98239206811a41078661e147e92589657397ba
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-09-01 14:22:10 -0400

    Time: 4531 ms (15.38%), Space: 13.8 MB (52.35%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -12,8 +12,17 @@ EDGE CASES:
         stringOne = Counter(s1)
         stringTwo = Counter(s2)
         
-        for i in range(len(s2) - len(s1) + 1): #how is it guaranteed that the last two characters of s2 wont have the permutation?
-            if stringOne == Counter(s2[i:i+len(s1)]):
+        if s1 in s2:
+            return True
+        
+        if len(s2) < len(s1):
+            return False
+        
+        if s1 == s2 and stringOne == stringTwo:
+            return True
+        
+        for i in range(len(s2) - len(s1) + 1): #this is needed so that later on we can only find the counter that only counts for the length of the window we are looking for
+            if stringOne == Counter(s2[i:i+len(s1)]): #the last number after the colon : gets ommitted
                 return True
         return False
             
