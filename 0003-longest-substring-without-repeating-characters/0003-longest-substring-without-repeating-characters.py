b27654d819748b95a09ddaca3ef124589cdea246
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-09-03 17:13:56 -0400

    Time: 62 ms (32.12%), Space: 13.6 MB (70.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,26 +1,34 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
-        :type s: str
-        :rtype: int
-        left = 0, right = 1
-        right < len(s):
-        if right != left:
-        expand the window len += 1
-        if right == left:
-        shrink the window 
+        Pseudocode:
+        1. return variable: maxLen = 0
+        2. two pointers to iterate the string -> left, right = 0, 0
+        3. Initialize a Counter to store character frequencies
+        4. while loop -> Ensure 'right' is within string bounds
+                a. if right character count is still empty == 0 from the substring, then:
+                        i. Update Counter for the current character by adding 1
+                        ii. Move the 'right' pointer forward by 1
+                        iii. find the maxLen = max(maxLen, right - left)  Update maxLen if needed
+                b. if left character count is exceeding more than 1 from the substring, then:
+                        i. Remove the character at 'left' from Counter
+                        ii.Move the 'left' pointer forward by 1
+            
+        5. return maxLen
+TC: O(n) SC: O(n)
         """
         maxLen = 0
-        left, right = 0,0
-        hashSet = set()
+        left, right = 0, 0
+        charCount = Counter()
+        
         while right < len(s):
-            while s[right] in hashSet:
-                hashSet.remove(s[left])
+            length = 0
+            count = 0
+            if charCount[s[right]] == 0:
+                charCount[s[right]] += 1
+                right += 1
+                maxLen = max(maxLen, right - left)
+            else:
+                charCount[s[left]] -= 1
                 left += 1
-            hashSet.add(s[right])
-            maxLen = max(maxLen, right - left + 1)
-            right += 1
         return maxLen
-            
-        
-        
