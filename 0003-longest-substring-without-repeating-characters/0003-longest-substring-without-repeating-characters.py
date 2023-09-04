334a944b55de31cc9350a970a6582b6a8bd2d95b
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-09-04 07:38:05 -0400

    Time: 73 ms (29.65%), Space: 13.5 MB (84.89%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -2,28 +2,25 @@ class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
         Pseudocode:
-        1. return variable: maxLen = 0
-        2. two pointers to iterate the string -> left, right = 0, 0
-        3. Initialize a Counter to store character frequencies
-        4. while loop -> Ensure 'right' is within string bounds
-                a. if right character count is still empty == 0 from the substring, then:
-                        i. Update Counter for the current character by adding 1
-                        ii. Move the 'right' pointer forward by 1
-                        iii. find the maxLen = max(maxLen, right - left)  Update maxLen if needed
-                b. if left character count is exceeding more than 1 from the substring, then:
-                        i. Remove the character at 'left' from Counter
-                        ii.Move the 'left' pointer forward by 1
-            
-        5. return maxLen
-TC: O(n) SC: O(n)
+        1. variable to return max
+        2. two pointers: left and right
+        3. each character count using Counter creation: O(n) but accessing O(1)
+        4. while loop: right cant surpass the length of s
+            a. if the right character count = 0:
+                character count of right += 1
+                right += 1
+                max = max(max, right - left)
+            b. else:
+                character count of left -= 1
+                left += 1
+        5. return the variable
+        TC: O(n) SC: O(1)
         """
         maxLen = 0
-        left, right = 0, 0
+        left, right = 0,0
         charCount = Counter()
         
         while right < len(s):
-            length = 0
-            count = 0
             if charCount[s[right]] == 0:
                 charCount[s[right]] += 1
                 right += 1
@@ -32,3 +29,6 @@ TC: O(n) SC: O(n)
                 charCount[s[left]] -= 1
                 left += 1
         return maxLen
+        
+        
+        
