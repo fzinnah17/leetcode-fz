c0805967e98c6ff007420aba2e90a75371870b6a
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-09-09 21:23:49 -0400

    Time: 104 ms (59.01%), Space: 16.3 MB (48.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -3,12 +3,40 @@ class Solution:
         rowSet = defaultdict(set) #()
         colSet = defaultdict(set) #()
         squareSet = defaultdict(set) #()
+        
+        #functions are for debugging processes -> easily identify what's wrong
+        #check from the board:
+        """
+        breaking the logic down
+            a. function(params):
+                    same logic but specific r,c,s
+                        False
+                    True
+        not function(params) -> False
+        """
+        #check row
+        def checkRow(r,num):
+            if num in rowSet[r]:
+                return False
+            return True
 
+        #check col
+        def checkCol(c,num):
+            if num in colSet[c]:
+                return False
+            return True
+        
+        #check box
+        def checkSquare(r,c,num):
+            if num in squareSet[(r//3,c//3)]:
+                return False
+            return True
+            
         for r in range(len(board)):
             for c in range(len(board)):
                 if board[r][c] == ".":
                     continue
-                if board[r][c] in rowSet[r] or board[r][c] in colSet[c] or board[r][c] in squareSet[(r//3,c//3)]:
+                if not(checkRow(r,board[r][c]) and checkCol(c,board[r][c]) and checkSquare(r,c,board[r][c])):
                     return False
                 rowSet[r].add(board[r][c])
                 colSet[c].add(board[r][c])
