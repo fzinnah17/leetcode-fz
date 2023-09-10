0ea3a66f5d508167c80f96bc03c6a3198c28b7b5
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-09-10 18:54:53 -0400

    Time: 64 ms (59.75%), Space: 16.4 MB (77.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,34 +1,19 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         """
-        Pseudocode:
-        1. variable to return max
-        2. two pointers: left and right
-        3. each character count using Counter creation: O(n) but accessing O(1)
-        4. while loop: right cant surpass the length of s
-            a. if the right character count = 0:
-                character count of right += 1
-                right += 1
-                max = max(max, right - left)
-            b. else:
-                character count of left -= 1
-                left += 1
-        5. return the variable
-        TC: O(n) SC: O(1)
         """
-        maxLen = 0
-        left, right = 0,0
-        charCount = Counter()
-        
+        res = 0
+        right = 0
+        left = 0
+        stringSet = set()
+
         while right < len(s):
-            if charCount[s[right]] == 0:
-                charCount[s[right]] += 1
+            if s[right] not in stringSet:
+                stringSet.add(s[right])
+                res = max(res, right - left + 1)
                 right += 1
-                maxLen = max(maxLen, right - left)
             else:
-                charCount[s[left]] -= 1
+                stringSet.remove(s[left])
                 left += 1
-        return maxLen
-        
-        
+        return res
         
