7dd1823ac7706b1c76e7a0099ef2945d497f4a9f
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-09-14 15:15:52 -0400

    Time: 36 ms (88.18%), Space: 16.8 MB (23.16%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,18 +1,22 @@
-class Solution(object):
-    def generateParenthesis(self, n):
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
         """
-        :type n: int
-        :rtype: List[str]
-        """
-        result = []
-        def helper(current, leftPar, rightPar):
-            if len(current) == 2*n:
-                result.append(current)
-                return
-            if leftPar < n:
-                helper(current + "(", leftPar + 1, rightPar)
-            if rightPar < leftPar:
-                helper(current + ")", leftPar, rightPar + 1)
-        helper("", 0, 0)
-        return result
+        TC: The worst-case scenario is when you have to generate all possible combinations of parenthesis. This is exponential and is approximately O(2^(2n)/sqrt(n)), based on the nth Catalan number. This is because there are 2^(2n) total sequences for n pairs of parentheses, but only the nth Catalan number of them are valid.
+        SC: The space required will also be exponential in the worst case, which is the number of valid sequences. So, O(2^(2n)/sqrt(n)).
+"""
+        res = []
+        
+        q = deque([('',0,0)])
+        
+        while q:
+            currStr, currOpen, currClose = q.popleft()
+            if currOpen == n and currClose == n:
+                res.append(currStr)
+            else:
+                if currOpen < n:
+                    q.append((currStr + '(', currOpen + 1, currClose))
+                if currOpen > currClose:
+                    q.append((currStr + ')', currOpen, currClose + 1))
+
+        return res
         
