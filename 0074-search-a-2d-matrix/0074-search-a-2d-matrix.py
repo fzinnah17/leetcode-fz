158a1bd43a6b5485f06e25db5d2d8df84c3eb484
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-10-22 12:44:07 -0400

    Time: 53 ms (34.81%), Space: 16.8 MB (41.85%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,12 +1,15 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row = len(matrix)
-        col = len(matrix[0])
+        r = len(matrix)
+        c = len(matrix[0])
+        
         left = 0
-        right = row * col - 1
+        right = r * c - 1
+        
         while left <= right:
-            middle = left + ((right - left) // 2)
-            middleIndex = matrix[middle // col][middle % col]
+            middle = left + ((right - left)//2)
+            middleIndex = matrix[middle // c][middle % c]
+            
             if middleIndex < target:
                 left = middle + 1
             elif middleIndex > target:
@@ -14,4 +17,7 @@ class Solution:
             else:
                 return True
         return False
+    """
+    TC: O(log(n * m))
+    SC: O(1)"""
         
