185e6d2110c7b92328672071826779651303bed0
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-10-24 11:29:59 -0400

    Time: 172 ms (5.18%), Space: 22.6 MB (95.89%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,26 +1,20 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def helper(r, target):
+        def helper(row, target):
             left = 0
-            right = len(r) - 1
+            right = len(row) - 1
             
             while left <= right:
                 middle = left + ((right - left) // 2)
-                if r[middle] < target:
+                if row[middle] < target:
                     left = middle + 1
-                elif r[middle] > target:
+                elif row[middle] > target:
                     right = middle - 1
                 else:
                     return True
             return False
-             
-        for row in matrix:
-            if helper(row, target):
+        for r in range(len(matrix)):
+            if helper(matrix[r], target):
                 return True
         return False
-    """
-    TC: binary search is O(logn), and since we perform binary search on all the rows, the final time complexity of the whole solution is O(nlogn).
-    SC: O(1)
-"""
-            
         
