19b73b73d6363c69f358a018a325bc22c87ac430
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-10-29 20:17:59 -0400

    Time: 53 ms (17.22%), Space: 16.5 MB (85.99%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,17 +1,15 @@
 class Solution:
-    """
-    TC: O(logn)
-    SC: O(1)"""
     def findMin(self, nums: List[int]) -> int:
         left = 0
         right = len(nums) - 1
         
-        while left < right: # loop continues until it zeroes in on the inflection point.
+        while left < right:
             middle = left + ((right - left) // 2)
-            if nums[middle] > nums[right]: #inflection point (minimum element) is to the right of middle, we're still in the descending part of the array.
+            
+            if nums[middle] > nums[right]:
                 left = middle + 1
-            else: #ascending part of the array, and the inflection point is to our left (or the middle point itself)
-                right = middle 
+            else:
+                right = middle
         return nums[right]
-    """
-     we're comparing the middle element with the rightmost element to determine which part of the array we're in (the strictly increasing part or the part that contains the inflection point)"""
+        
+        
