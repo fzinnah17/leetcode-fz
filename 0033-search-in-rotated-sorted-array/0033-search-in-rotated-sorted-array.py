b0620d3403937a563fbe2405708b55d644f40e9b
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-10-30 21:46:05 -0400

    Time: 26 ms (30.30%), Space: 13.5 MB (63.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -9,21 +9,19 @@ class Solution(object):
         right = len(nums) - 1
         
         while left <= right:
-            middle = left + ((right - left) // 2)
-            #nums = [4,5,6,7,0,1,2], target = 5
+            #nums = [4,5,6,7,0,1,2], target = 0
+            middle = left + ((right - left) // 2) #middle = 7
             if nums[middle] == target:
                 return middle
-            if nums[middle] >= nums[left]: #check if target is in the left side or to the right side
+            if nums[middle] < nums[left]: #[4,5,6,7] the answer is not in the left portion
+                if nums[middle] > target or target > nums[right]: #not in left portion
+                    right = middle - 1
+                else:
+                    left = middle + 1
+            else: #if nums[middle] < nums[left]  nums = [4,5,6,7,0,1,2,3,4]
                 if nums[middle] < target or target < nums[left]:
                     left = middle + 1
                 else:
                     right = middle - 1
-            else:
-                #if nums[middle] < nums[left]
-                if nums[middle] > target or target > nums[right]:
-                    right = middle - 1
-                else:
-                    left = middle + 1
         return -1
-                
         
