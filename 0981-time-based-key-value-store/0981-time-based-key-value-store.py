faada5b0d94d3cfe5dab7f420263b2001a86b13b
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-05 15:06:31 -0500

    Time: 718 ms (20.08%), Space: 73.9 MB (71.09%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,43 +1,37 @@
-class TimeMap: #object/result = TimeMap
+class TimeMap:
 
     def __init__(self):
         self.dict = {}
         
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        #more of like a hashmap key :value value: key
         if key not in self.dict:
             self.dict[key] = []
-        self.dict[key].append([timestamp,value])
-        
+        self.dict[key].append([value,timestamp]) 
+    
     def get(self, key: str, timestamp: int) -> str:
-        #assumes that set was already called
-        #previous time <= current time
-        #multiple vales -> return largest previous time aka recent
-        #no value match ->  ""
+        #think the timestamp as the target here
+        res = ""
         if key not in self.dict:
             return ""
-        time_array = self.dict[key]
-        
-        left = 0
-        right = len(time_array) - 1
+        value_list = self.dict[key]
+        # print(value_list)
+        # [['bar', 1]]
+        # [['bar', 1]]
+        # [['bar', 1], ['bar2', 4]]
+        # [['bar', 1], ['bar2', 4]]
+        left, right = 0, len(value_list) - 1
         
         while left <= right:
             middle = left + ((right - left) // 2)
             
-            if time_array[middle][0] == timestamp:
-                return time_array[middle][1]
-            elif time_array[middle][0] < timestamp:
+            if value_list[middle][1] <= timestamp:
+                res = value_list[middle][0] #update with the value
                 left = middle + 1
             else:
                 right = middle - 1
-        if right < 0 or time_array[right][0] > timestamp:
-            return ""
-        return time_array[right][1]        
+        return res
         
-        
-        
-
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
