33a4ff140137a5a7b671970f2aaa195afd6f34df
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-05 13:53:14 -0500

    Time: 27 ms (20.79%), Space: 13.6 MB (29.67%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,23 +5,23 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        left = 0
-        right = len(nums) - 1
-        
+        left, right = 0, len(nums) - 1
         while left <= right:
-            #nums = [4,5,6,7,0,1,2], target = 0
-            middle = left + ((right - left) // 2) #middle = 7
+            middle = left + ((right - left) // 2)
+            
+            if nums[middle] >= nums[left]:
+                if target > nums[middle] or target < nums[left]:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+            else:
+                if target < nums[middle] or target > nums[right]:
+                    right = middle - 1
+                else:
+                    left = middle + 1
             if nums[middle] == target:
                 return middle
-            if nums[middle] < nums[left]: #[4,5,6,7] the answer is not in the left portion
-                if nums[middle] > target or target > nums[right]: #not in left portion
-                    right = middle - 1
-                else:
-                    left = middle + 1
-            else: #if nums[middle] < nums[left]  nums = [4,5,6,7,0,1,2,3,4]
-                if nums[middle] < target or target < nums[left]:
-                    left = middle + 1
-                else:
-                    right = middle - 1
-        return -1
+        return - 1
+                
+        
         
