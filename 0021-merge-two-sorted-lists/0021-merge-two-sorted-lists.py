8d0295c3209e9b0c7bbed8a01c39537a156e5845
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-07 11:08:12 -0500

    Time: 53 ms (6.67%), Space: 16.1 MB (98.05%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,33 +1,51 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        TC: O(N) SC: O(N)
-        """
-        temp = ListNode(0)
-        current = temp #Initialize Pointers and Variables
+class Solution:
+    def mergeTwoLists(self, list1Head: Optional[ListNode], list2Head: Optional[ListNode]) -> Optional[ListNode]:
+        #top pointer = list1head
+        #bottom pointer = list2head
+        #while both exists: compare the values of both list nodes
+        #if l1 == l2: move the l1 pointer
+        #if l1 > l2: 
+        prev = None
+        top = list1Head
+        bottom = list2Head
         
-        if not list1 and not list2:
-            return None #Handle Edge Cases
+        if not top:
+            return bottom
+        if not bottom:
+            return top
         
-        list1Ptr = list1 #Initialize Pointers and Variables
-        list2Ptr = list2 
+        #which list to start from?
+        if top.val < bottom.val:
+            prev = top
+            top = top.next
+        else: #top.val => bottom.val
+            prev = bottom
+            bottom = bottom.next #change the position
+        head = prev #Return the head of the merged linked list.
         
-        while list1Ptr and list2Ptr: #Traverse the Linked List
-            if list1Ptr.val < list2Ptr.val: #Manipulate Pointers and Nodes
-                current.next = list1Ptr #Keep Track of Relevant Information(current connects to other list values and the position of it moves according to the conditional statements)
-                list1Ptr = list1Ptr.next
+        while top and bottom:
+            if top.val <= bottom.val:
+                temp = top.next
+                prev.next = top
+                prev = top
+                top = temp
+                
             else:
-                current.next = list2Ptr
-                list2Ptr = list2Ptr.next #Implement the Solution Logic continues until one of the input lists is exhausted
-            current = current.next
-        if list1Ptr: #still has remaining nodes after the loop
-            current.next = list1Ptr
-        if list2Ptr:
-            current.next = list2Ptr
-        return temp.next
-                   
+                temp = bottom.next
+                prev.next = bottom
+                prev = bottom
+                bottom = temp
+        
+        if top:
+            prev.next = top
+        else:
+            prev.next = bottom
+        return head
+            
+            
+                
