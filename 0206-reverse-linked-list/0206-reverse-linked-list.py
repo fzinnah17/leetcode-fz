557f96ca8369d6b34417c52e5b06b8c5eda5d03f
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-07 07:10:31 -0500

    Time: 40 ms (71.02%), Space: 17.8 MB (47.71%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,17 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #manipulate the references of the nodes -> two pointer
-        #take two pointers one at head and other at NULL position intially and we slowly reverse the point position at each point using a temp variable and solve the problem using normal iteration
-        prev = None
-        curr = head
-        
+        prev, curr = None, head
         while curr:
             temp = curr.next
             curr.next = prev
             prev = curr
             curr = temp
         return prev
-        
-        
+    #TC: O(n) SC: O(1)
         
