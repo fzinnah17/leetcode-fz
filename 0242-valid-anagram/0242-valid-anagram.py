dee82958dbd4f1b444df49a90de0b42e55e80377
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-12 16:00:51 -0500

    Time: 53 ms (62.97%), Space: 16.8 MB (63.60%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,13 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        TC: O(n) SC: O(n)
-        """
-        if Counter(s) == Counter(t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sMap = defaultdict(int)
+        tMap = defaultdict(int)
+        # print(sMap, tMap)
+        for i in range(len(s)):
+            sMap[s[i]] += 1
+        for i in range(len(t)):
+            tMap[t[i]] += 1
+        #print(sMap, tMap)
+        if sMap == tMap:
             return True
         return False
