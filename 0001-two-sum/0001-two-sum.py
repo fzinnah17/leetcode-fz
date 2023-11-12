f85405fb014b1eaf36b949c39e66849482c268d2
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-12 11:45:28 -0500

    Time: 53 ms (97.54%), Space: 17.4 MB (53.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,23 +1,13 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        Pseudocode:
-        1. Create a hash table/dict
-        value : index
-        2. iterate over the nums array to have both index and the value and fill in the hash table
-                a. if the values are found, return:
-                        [dictionary[found value], current index]
-                b. if not found, then assign:
-                        dictionary[value] = index
-        TC: O(n) for loop SC: O(n) dictionary
-        """
-        numDict = {} #key = nums[i] = v value = i(index)
-        # value : index
-        for index, value in enumerate(nums):
-            complement = target - value #complement is another value that I am looking for so treat it as a regular value
-            if complement in numDict:
-                return [numDict[complement], index]
-            numDict[value] = index
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numDict = {}
+        
+        for i in range(len(nums)):
+            comp = target - nums[i]
             
+            if comp not in numDict:
+                numDict[nums[i]] = i
+            else:
+                return [i, numDict[comp]]
         
         
