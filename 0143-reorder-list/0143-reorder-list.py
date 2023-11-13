4d9ea088c2db842e2651e2d328aefa5d77f8b74a
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-13 00:24:06 -0500

    Time: 72 ms (87.21%), Space: 26.2 MB (26.55%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -1,54 +1,41 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reorderList(self, head):
-        """
-        :type head: ListNode
-        :rtype: None Do not return anything, modify head in-place instead.
-        """
-        def reverseList(head):
-            prev = None
-            curr = head
-            while curr:
-                nxt = curr.next
-                curr.next = prev
-                prev = curr
-                curr = nxt
-            return prev 
-        def midElement(head):
-            length = 0
-            start = head
-            while start:
-                length += 1
-                start = start.next
-            left = 0
-            middle = length // 2
-            while head:
-                if left == middle:
-                    return head
-                else:
-                    left += 1
-                    head = head.next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
             return None
+
+        # Step 1: Find the middle of the linked list
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # # Step 2: Reverse the second half of the linked list
+        # prev, curr = None, slow.next #4
+        # slow.next = None #the first of the list is being broken from the other part of the list
+        # while curr:
+        #     curr.next = prev #4.next = null
+        #     prev = curr #none = 4 move to the right
+        #     curr = curr.next #curr becomes 5 and repeat this process
+        # Step 2: Reverse the second half of the linked list
+        prev, curr = None, slow.next
+        slow.next = None
+        while curr:
+            next_temp = curr.next  # Save the next node
+            curr.next = prev       # Reverse the current node's pointer
+            prev = curr            # Move prev to the current node
+            curr = next_temp       # Move to the next node
+
+
+        # Step 3: Merge the two halves
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
         
-        firstHalf = midElement(head)
-        secondHalf = firstHalf.next #initialize
-        firstHalf.next = None #assigning pointers
-        secondHalf = reverseList(secondHalf)
-        firstPointer = head
-        secondPointer = secondHalf
-        while firstPointer and secondPointer:
-            firstNext = firstPointer.next
-            firstPointer.next = secondPointer
-            secondNext = secondPointer.next
-            secondPointer.next = firstNext
-            firstPointer = firstNext
-            secondPointer = secondNext
-        return head
-    #TC: O(n) SC: O(1)
-            
-            
-            
