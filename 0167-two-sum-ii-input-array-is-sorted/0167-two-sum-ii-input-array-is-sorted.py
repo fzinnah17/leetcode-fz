a46471459cbc62169bd9a862ce1068a7f5621975
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-22 21:32:33 -0500

    Time: 95 ms (25.41%), Space: 14.3 MB (13.36%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,45 +1,14 @@
 class Solution(object):
-    def twoSum(self, numbers, target):        
-        """
-        pointers = bounds   ----> left, right
-        middle
-        if middle > target : right -= 1
-        if middle < target: left += 1
-        THIS IS A 1-INDEXED ARRAY SO GOTTA ADD 1 WITH THE INDICES
-        """
-        
-        for i in range(len(numbers)):
-            newTarget = target - numbers[i]
-            left = i + 1
-            right = len(numbers) - 1
-            while left <= right:
-                middle = left + ((right - left)//2)
-
-                if numbers[middle] > newTarget:
-                    right = middle - 1
-
-                elif numbers[middle] < newTarget:
-                    left = middle + 1
-                else:
-                    return [i+ 1, middle + 1]
-                
-#         #TC: O(nlogn)  SC: O(1)
-
-
-
-
-"""ANOTHER APPROACH WITH O(N) TIME AND O(1) SPACE:
+    def twoSum(self, numbers, target):
         left, right = 0, len(numbers) - 1
-        while left <= right:
-            addition = numbers[left] + numbers[right]
-            if addition > target:
+        
+        while left < right:
+            curr = numbers[left] + numbers[right]
+            
+            if curr > target:
                 right -= 1
-            elif addition < target:
+            elif curr < target:
                 left += 1
             else:
-                return [left + 1, right + 1] #because this is a 1-indexed array
-        #TC: O(N) SC: O(1)
-        """
-        
-            
+                return [left + 1, right + 1]
         
