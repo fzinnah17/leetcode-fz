c389fa0111fc8c0350f7a40864a3c0a939bcb62d
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-22 21:46:48 -0500

    Time: 820 ms (68.15%), Space: 16.7 MB (49.72%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,32 @@
 class Solution(object):
     def threeSum(self, nums):
         """
+        [-4, -1, -1, 0, 1, 2]
+        [[-1,-1,2],[-1,0,1]]
         """
         nums.sort()
-        result = []
-        for ind, num in enumerate(nums):
-            if num > 0:
-                break
-            if ind > 0 and num == nums[ind - 1]:
+        res = [] #beacuse returning list inside of a list
+        
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            left = ind+1
-            right = len(nums) - 1
-            while (left < right):
-                sum = num + nums[left] + nums[right]
-                if sum < 0:
-                    left += 1
-                elif sum > 0:
-                    right -=1
-                else:
-                    result.append([num, nums[left], nums[right]])
-                    left += 1 
+            
+            left, right = i + 1, len(nums) - 1
+            
+            while left < right:
+                curr = nums[left] + nums[right] + nums[i]
+                if curr > 0:
                     right -= 1
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1
-        return result
+                elif curr < 0:
+                    left += 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                
+                    while left < right and nums[left] == nums[left - 1]:
+                        # if nums[left] == nums[left + 1]:
+                            left += 1
+        return res
+                
+                
         
