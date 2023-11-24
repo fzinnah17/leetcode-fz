0e029b17e4cac5347f887c336b20d1fc0166bbf3
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-24 09:34:34 -0500

    Time: 63 ms (15.83%), Space: 16.9 MB (30.60%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # sMap = defaultdict(int)
-        # tMap = defaultdict(int)
-        sMap = {}
+        sMap ={} #key = character aka value
         tMap = {}
+        
         for i in range(len(s)):
             if s[i] not in sMap:
                 sMap[s[i]] = 1
-            sMap[s[i]] += 1
+            else:
+                sMap[s[i]] += 1
         for i in range(len(t)):
             if t[i] not in tMap:
                 tMap[t[i]] = 1
-            tMap[t[i]] += 1
-        #print(sMap, tMap)
-        if sMap == tMap:
-            return True
-        return False
+            else:
+                tMap[t[i]] += 1
+        """
+        {'a': 3, 'n': 1, 'g': 1, 'r': 1, 'm': 1}
+
+        {'n': 1, 'a': 3, 'g': 1, 'r': 1, 'm': 1}
+"""
+        
+        if sMap != tMap:
+            return False
+        return True
+                
+                
