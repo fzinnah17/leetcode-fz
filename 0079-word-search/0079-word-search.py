f5223d9c2dedcc52267bb9208ca0bc01d4a20eee
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-24 10:50:56 -0500

    Time: 5839 ms (49.45%), Space: 16.5 MB (12.01%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,32 +3,34 @@ class Solution:
         Rows = len(grid)
         Cols = len(grid[0])
         visited = set()
-        
+
+        # Define a recursive depth-first search (DFS) function.
         def dfs(r, c, pointer):
+            # Check if we've matched the entire word.
             if len(word) == pointer:
                 return True
-            if(r < 0 or c < 0
-              or r >= Rows or c >= Cols
-              or (r,c) in visited
-              or grid[r][c] != word[pointer]):
+            # Check for boundary conditions and visited cells.
+            if (r < 0 or c < 0
+                or r >= Rows or c >= Cols
+                or (r, c) in visited
+                or grid[r][c] != word[pointer]):
                 return
-            visited.add((r,c))
+
+            visited.add((r, c))
+            # Recursively search in all four directions.
             res = (dfs(r + 1, c, pointer + 1) or
-                  dfs(r - 1, c, pointer + 1) or
-                  dfs(r, c + 1, pointer + 1) or
-                  dfs(r, c- 1, pointer + 1))
-            visited.remove((r,c))
+                   dfs(r - 1, c, pointer + 1) or
+                   dfs(r, c + 1, pointer + 1) or
+                   dfs(r, c - 1, pointer + 1))
+            visited.remove((r, c))
             return res
-        
+
+        # Iterate through the grid to find the starting point for the word.
         for row in range(Rows):
             for col in range(Cols):
                 if grid[row][col] == word[0]:
+                    # Start DFS from this point.
                     if dfs(row, col, 0):
                         return True
+        # If we didn't find the word starting from any point, return False.
         return False
-                    
-                    
-                
-        
-        
-        
