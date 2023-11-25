09b585ea0f1e7e2e7426b17489495ec3895066a7
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-25 16:06:32 -0500

    Time: 194 ms (48.13%), Space: 17 MB (88.26%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -1,22 +1,19 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.streamMap = {}
-        self.pointer = 1
+        self.messagesMap = [None] * (n + 1)  # Initialize a list to store messages
+        self.ptr = 1  # Initialize the pointer to 1
 
     def insert(self, idKey: int, value: str) -> List[str]:
         res = []
-        
-        self.streamMap[idKey] = value
-        
-        while self.pointer in self.streamMap:
-            res.append(self.streamMap[self.pointer])
-            self.pointer += 1
-        
+
+        # Store the message at the given idKey
+        self.messagesMap[idKey] = value
+
+        # Check if the message at the current pointer is not None
+        while self.ptr < len(self.messagesMap) and self.messagesMap[self.ptr]:
+            # Append the message to the result and increment the pointer
+            res.append(self.messagesMap[self.ptr])
+            self.ptr += 1
+
         return res
-        
-
-
-# Your OrderedStream object will be instantiated and called as such:
-# obj = OrderedStream(n)
-# param_1 = obj.insert(idKey,value)
