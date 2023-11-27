914cfd8dfef34d142f406ebea824c56656664235
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-27 02:06:51 -0500

    Time: 164 ms (22.19%), Space: 17 MB (59.61%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -4,22 +4,38 @@ class Solution:
         hmap = {}
         invalid_indices = set()
         
-        for i, t in enumerate(trans):
+        i = 0
+        while i < len(trans):
+            t = trans[i]
             name, time, amount, city = t.split(',')
+            
             if name not in hmap:
                 hmap[name] = []
+                
             hmap[name].append((i, int(time), int(amount), city))
+            i += 1
 
-#         alice -> [[1, 20, 800, mtv], []]
         for name, transactions in hmap.items():
-            for i, (idx1, time1, amount1, city1) in enumerate(transactions):
+            j = 0
+            while j < len(transactions):
+                idx1, time1, amount1, city1 = transactions[j]
                 
                 if amount1 > 1000:
                     invalid_indices.add(idx1)
                     
-                for j, (idx2, time2, amount2, city2) in enumerate(transactions):
-                    if i != j and abs(time1 - time2) <= 60 and city1 != city2:
+                k = 0
+                while k < len(transactions):
+                    idx2, time2, amount2, city2 = transactions[k]
+                    
+                    if j != k and abs(time1 - time2) <= 60 and city1 != city2:
                         invalid_indices.add(idx1)
                         invalid_indices.add(idx2)
+                    
+                    k += 1
+                
+                j += 1
             
-        return [trans[i] for i in invalid_indices]
+        invalid_transactions = []
+        for i in invalid_indices:
+            invalid_transactions.append(trans[i])
+        return invalid_transactions
