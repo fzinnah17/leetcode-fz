d276868d8a5217ddc3662e97ea6d0f134dd5c794
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-26 22:55:18 -0500

    Time: 220 ms (85.89%), Space: 27.9 MB (11.04%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,54 +1,29 @@
-# class UndergroundSystem:
+class UndergroundSystem:
 
-#     def __init__(self):
-        
+    def __init__(self):
+        self.checkinMap = {}
+        self.travelMap = {} #key = route taken, value = totalTime, count
 
-#     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        
+    def checkIn(self, passengerId: int, checkInstation: str, checkIntime: int) -> None:
+        self.checkinMap[passengerId] = (checkInstation, checkIntime)   
 
-#     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        
+    def checkOut(self, passengerId: int, checkOutstation: str, checkOuttime: int) -> None:
+        startStation, startTime = self.checkinMap[passengerId]
+        route = (startStation, checkOutstation)
+        totalTime = checkOuttime - startTime
+        if route not in self.travelMap:
+            self.travelMap[route] = [0,0]
+        self.travelMap[route][0] += totalTime
+        self.travelMap[route][1] += 1
 
-#     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        
-
-
-# # Your UndergroundSystem object will be instantiated and called as such:
-# # obj = UndergroundSystem()
-# # obj.checkIn(id,stationName,t)
-# # obj.checkOut(id,stationName,t)
-# # param_3 = obj.getAverageTime(startStation,endStation)
-
-class UndergroundSystem(object):
-    '''Station1 - Station2 - Station3 '''
-    """"""
-    ''''''
-    def __init__(self): #SC: O(n)
-        self.startStation = {}
-        self.total = {}
-
-    def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.startStation[id] = (stationName, t)  # T-O(1)
-        return
-    def checkOut(self, id: int, endStation: str, t: int) -> None:
-        checkin_station, checkin_time = self.startStation[id]
-        del self.startStation[id] #S-O(1)
-        # (start_station, end_station) = [timeEnd - timeStart, toalJourney+1] 
-        if (checkin_station, endStation) in self.total:
-            self.total[(checkin_station, endStation)][0] += 1
-            self.total[(checkin_station, endStation)][1] += (t-checkin_time)
-        else:
-            self.total[(checkin_station, endStation)] = [1, (t-checkin_time)]
-    def getAverageTime(self, checkin_station: str, endStation: str) -> float:
-        # average = total value/ number of values
-        total_count, total_time = self.total[(checkin_station, endStation)]
-        
-        return total_time/total_count
-        
-
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        route = (startStation, endStation)
+        time, count = self.travelMap[route]
+        avgTime = time / count
+        return avgTime
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
-# obj.checkIn(id,stationName,t)
-# obj.checkOut(id,stationName,t)
+# obj.checkIn(passengerId,checkInstation,checkIntime)
+# obj.checkOut(passengerId,checkOutstation,checkOuttime)
 # param_3 = obj.getAverageTime(startStation,endStation)
