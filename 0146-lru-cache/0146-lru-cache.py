d14c23252a7489d0644e7f68249c6013a74a1ac8
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-27 12:53:30 -0500

    Time: 736 ms (22.24%), Space: 78.5 MB (17.24%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,77 +1,63 @@
-class DLL():
+class DLL:
+    
     def __init__(self, key, val):
         self.key = key
         self.val = val
         self.prev = None
         self.next = None
-
+    
 class LRUCache:
 
     def __init__(self, capacity: int):
-        """
-        hashmap key = key, value = node reference
-        DLL uage of the order
-        """
-        self.cacheMap = {} #key= key, val = node
+        self.cap = capacity
+        self.cacheMap = {}
         self.head = DLL(0, 0)
         self.tail = DLL(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.capacity = capacity
         
-    
+    """
+    head <> [] <> [node] <> [] <> tail
+    """
     def removeNode(self, node):
-        ''' head <> [] <> [node] <> [] <> tail '''
+        """give the variable names to the nodes"""
         prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-        
+        nxt.prev, prev.next = prev, nxt
         
     def addNode(self, node):
-        ''' head <> [] <> [] <> [node] <> tail '''
+        """
+        give the variable names to the nodes
+                         prev
+            head <> [] <> [] <> tail
+            head <> [] <> [] <> [node] <> tail"""
         prev = self.tail.prev
-        prev.next = node
         self.tail.prev = node
+        prev.next = node
         node.next = self.tail
         node.prev = prev
         
-
+        
     def get(self, key: int) -> int:
         if key in self.cacheMap:
             node = self.cacheMap[key]
             self.removeNode(node)
             self.addNode(node)
-            return node.val
+            nodeVal = node.val
+            return nodeVal     
         return -1
         
-
     def put(self, key: int, value: int) -> None:
-        """
-        check hashmap
-        -- find
-        -- check the capactiy
-        --- i) under the cap
-        --- ii) exceed the cap
-        
-        cap 2
-        head <> [] <> [] <> [] <> tail
-            
-            
-        
-        """
         if key in self.cacheMap:
+            #update the value of the key -> remove the old one
             self.removeNode(self.cacheMap[key])
+        #add the key-value pair to the cache if it doesn't exist or if we have to update
         self.cacheMap[key] = DLL(key, value)
         self.addNode(self.cacheMap[key])
-        
-        # check cap
-            # if <: remove from head
-        if len(self.cacheMap) > self.capacity:
+        # If the number of keys exceeds the capacity from this operation
+        if len(self.cacheMap) > self.cap:
             lru = self.head.next
             self.removeNode(lru)
             del self.cacheMap[lru.key]
-            
-            
-            
         
 
 
