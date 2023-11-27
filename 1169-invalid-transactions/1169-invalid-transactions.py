0bd9dce090a25bb6e83647fbe6436b83376fcb41
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-27 15:29:13 -0500

    Time: 68 ms (75.80%), Space: 17.1 MB (42.20%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,56 +1,22 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        invalid = []
+        transactionList = defaultdict(list)
         
-        # Map to store transactions grouped by name
-        transactions_by_name = {}
-        
-        # Set to store indices of invalid transactions
-        invalid_transaction_indices = set()
-        
-        # Step 1: Group transactions by name
-        current_index = 0
-        while current_index < len(transactions):
-            transaction = transactions[current_index]
-            name, time, amount, city = transaction.split(',')
-            
-            # If the name is not in the map, add it with an empty list
-            if name not in transactions_by_name:
-                transactions_by_name[name] = []
-                
-            # Append the current transaction to the list for the specific name
-            transactions_by_name[name].append((current_index, int(time), int(amount), city))
-            
-            # Move to the next transaction
-            current_index += 1
+        for i in transactions:
+            name, time, amount, city = i.split(",")
+            transactionList[name].append([time, amount, city])
 
-        # Step 2: Check for invalid transactions
-        for name, transactions_for_name in transactions_by_name.items():
-            first_transaction_index = 0
-            while first_transaction_index < len(transactions_for_name):
-                idx1, time1, amount1, city1 = transactions_for_name[first_transaction_index]
-                
-                # Condition 1: Check for amount greater than 1000
-                if amount1 > 1000:
-                    invalid_transaction_indices.add(idx1)
-                    
-                second_transaction_index = 0
-                while second_transaction_index < len(transactions_for_name):
-                    idx2, time2, amount2, city2 = transactions_for_name[second_transaction_index]
-                    
-                    # Condition 2: Check for time difference and different cities
-                    if first_transaction_index != second_transaction_index and abs(time1 - time2) <= 60 and city1 != city2:
-                        invalid_transaction_indices.add(idx1)
-                        invalid_transaction_indices.add(idx2)
-                    
-                    # Move to the next transaction
-                    second_transaction_index += 1
-                
-                # Move to the next transaction
-                first_transaction_index += 1
-            
-        # Step 3: Collect invalid transactions
-        invalid_transactions = []
-        for idx in invalid_transaction_indices:
-            invalid_transactions.append(transactions[idx])
-            
-        return invalid_transactions
+        for name, transactions in transactionList.items():
+            for trans in transactions:
+                t, a, c = trans
+                if int(a) > 1000:
+                    invalid.append(",".join([name, t, a, c]))
+                else:
+                    for other_trans in transactions:
+                        ot, oa, oc = other_trans
+                        if oc != c and abs(int(ot) - int(t)) <= 60:
+                            invalid.append(",".join([name, t, a, c]))
+                            break
+
+        return invalid
