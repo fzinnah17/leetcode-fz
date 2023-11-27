d31a32944faf24212ac8e3f7b10f0b9510e1affa
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-11-26 21:20:43 -0500

    Time: 727 ms (27.67%), Space: 78.4 MB (34.08%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,53 +1,79 @@
-class Node:
+class DLL():
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.next = None
         self.prev = None
+        self.next = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.head = Node(-1, -1)
-        self.tail = Node(-1, -1)
+        """
+        hashmap key = key, value = node reference
+        DLL uage of the order
+        """
+        self.cacheMap = {} #key= key, val = node
+        self.head = DLL(0, 0)
+        self.tail = DLL(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.m = {}
-
-    def addnode(self, newnode):
-        temp = self.head.next
-        newnode.next = temp
-        newnode.prev = self.head
-        self.head.next = newnode
-        temp.prev = newnode
-
-    def deletenode(self, delnode):
-        delprev = delnode.prev
-        delnext = delnode.next
-        delprev.next = delnext
-        delnext.prev = delprev
+        self.capacity = capacity
+        
+    
+    def removeNode(self, node):
+        ''' head <> [] <> [node] <> [] <> tail '''
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+        
+        
+    def addNode(self, node):
+        ''' head <> [] <> [] <> [node] <> tail '''
+        prev = self.tail.prev
+        prev.next = node
+        self.tail.prev = node
+        node.next = self.tail
+        node.prev = prev
+        
 
     def get(self, key: int) -> int:
-        if key in self.m:
-            resnode = self.m[key]
-            res = resnode.val
-            del self.m[key]
-            self.deletenode(resnode)
-            self.addnode(resnode)
-            self.m[key] = self.head.next
-            return res
+        if key in self.cacheMap:
+            node = self.cacheMap[key]
+            self.removeNode(node)
+            self.addNode(node)
+            return node.val
         return -1
+        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.m:
-            existingnode = self.m[key]
-            del self.m[key]
-            self.deletenode(existingnode)
-        if len(self.m) == self.cap:
-            del self.m[self.tail.prev.key]
-            self.deletenode(self.tail.prev)
-        self.addnode(Node(key, value))
-        self.m[key] = self.head.next
+        """
+        check hashmap
+        -- find
+        -- check the capactiy
+        --- i) under the cap
+        --- ii) exceed the cap
+        
+        cap 2
+        head <> [] <> [] <> [] <> tail
+            
+            
+        
+        """
+        if key in self.cacheMap:
+            self.removeNode(self.cacheMap[key])
+        self.cacheMap[key] = DLL(key, value)
+        self.addNode(self.cacheMap[key])
+        
+        # check cap
+            # if <: remove from head
+        if len(self.cacheMap) > self.capacity:
+            lru = self.head.next
+            self.removeNode(lru)
+            del self.cacheMap[lru.key]
+            
+            
+            
+        
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
