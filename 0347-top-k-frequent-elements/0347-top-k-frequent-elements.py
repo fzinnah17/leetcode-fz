7ee66661bc976ea23d58ec645d6771807b7bdbad
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-04 12:59:37 -0500

    Time: 89 ms (93.73%), Space: 20.9 MB (90.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,25 +1,19 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        Pseudocode:
-        1. Count all the numbers of the array TC: O(n)
-        2. Find the most common numbers based on the given parameter 'k' (value, #occurences) 
-        TC: O(n) SC: O(n) as it is returning a list
-        3. create an empty array to add the values to return later SC : O(n)
-        4. For loop to go over the most common number list TC: O(n)
-            a. append the first index of the tuples in that new array
-        5. Return the result array
-        
-        TC: O(n) + O(n) + O(n): O(n) = O(n) 
-        SC: O(n) + O(n) = O(n)
-        """
-        c = collections.Counter(nums)
-        commonNum = c.most_common(k)
-        # print(c.most_common(k)) [(1,3), (2,2)]
-        result = []
-        for i in commonNum:
-            result.append(i[0])
-        return result
-            
-        # return [i[0] for i in commonNum] list comprehension line
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        nMap = defaultdict(int)
+        for e in nums:
+            if e not in nMap:
+                nMap[e] = 1
+            else:
+                nMap[e] += 1
+        # print(nMap)
+        topK = sorted(nMap, key = nMap.get, reverse = True)[:k]
+        # return 
+        # print(nMap)
+        # print()
+        # print(topK)
+        return topK
+
+        # res = list(nMap.keys())[:k]
+        # return res
         
