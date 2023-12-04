95f6f2201aa8ec96e8671b668bf507a4f09c112a
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-04 12:07:13 -0500

    Time: 102 ms (48.74%), Space: 19.9 MB (61.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,20 @@
 class Solution:
-    def groupAnagrams(self, s: List[str]) -> List[List[str]]:
-        strMap = {} #key = tuple(sorted char) value = [words]
-        for word in s:
-            sortedWord = "".join(sorted(word)) #aet,aet,ant,aet,ant,abt
-            if sortedWord not in strMap:
-                strMap[sortedWord] = [word]
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        1. Create an empty dictionary to store anagram groups.
+        2. For each word in the input:
+           a. Sort the words and join them back together
+           b. Check if it is already a key in map.
+               I. If not, create a new entry as the key and a list containing as the value.
+               II. If already in map, append `word` to the list of values associated with that key.
+        3. After processing all words, return a list containing the values (lists of words) from the dictionary.
+"""
+        sMap = {}
+        for w in strs:
+            sortStrs = "". join(sorted(w))
+            if sortStrs not in sMap:
+                sMap[sortStrs] = [w]
             else:
-                strMap[sortedWord].append(word)
-        return list(strMap.values())
+                sMap[sortStrs]. append(w)
+        return sMap.values()
         
