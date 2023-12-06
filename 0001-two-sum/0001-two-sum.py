99c4de39d89d5d021efa3d80bfe24b92330ef43a
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-06 13:34:51 -0500

    Time: 58 ms (89.11%), Space: 17.6 MB (22.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numsMap = {}
+        nMap ={} #key = v, value = index
         for i in range(len(nums)):
             comp = target - nums[i]
-            if comp not in numsMap:
-                numsMap[nums[i]] = i
+            if comp not in nMap:
+                nMap[nums[i]] = i
             else:
-                return [i,numsMap[comp]]
-        #  Time: O(n) Space: O(n)
+                return [i, nMap[comp]]
+        
         
