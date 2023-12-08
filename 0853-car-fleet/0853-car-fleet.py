db6027ec220bc7d65e21573c0efae8488bc8f375
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-08 16:18:03 -0500

    Time: 758 ms (61.68%), Space: 46.7 MB (5.03%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,35 +1,32 @@
-class Solution(object):
-    from collections import deque
-    def carFleet(self, target, position, speed):
+class Solution:
+    def carFleet(self, target: int, pos: List[int], speed: List[int]) -> int:
         """
-        :type target: int
-        :type position: List[int]
-        :type speed: List[int]
-        :rtype: int
-        https://leetcode.com/problems/car-fleet/discuss/1335749/Python-3-Visual-Examples
+        {
+        10 : 2
+        8 : 4
+        0 : 1
+        5 : 1
+        3 : 3}
+    check if k + v is same to any other k + v, then fleet += 1
+    even if not fleet += 1 as car itself is a fleet
         """
-        #1. In a list, add the position and the speed:
-        carVals = []
-        for i in range(len(position)):
-            carVals.append([position[i],speed[i]])
-        #2. Sort the list from right to left according to the position because we need to reach a target mile which is in reverse bc usually we go left to right
-        carVals.sort(reverse = True) #[[10, 2], [8, 4], [5, 1], [3, 3], [0, 1]]
-        stack = deque()
-        #3. iterate through the list:
-        for pos, speed in carVals:
-            #4. Calculate and record time for each car to reach destination in the stack:
-            time = float(float((target - pos)) / float(speed))
-            stack.append(time)
-            # #5. Because I am adding right to left and right is going to my stack's bottom and left is coming on top of the stack
-            # """rightCarbottom = stack[-2]
-            # leftCartop = stack[-1]"""
-            # #6. Check if the fleets overlap. If so, pop it out
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
-                #7. Remove the top aka lowest time
-                stack.pop()
-        #8. Return the cars that never overlapped
-        return len(stack)
-        #TC: O(nlogn) SC:O(n)
-            
-            
+        cMap = {}
+        res = 0
         
+        for i in range(len(pos)):
+            key = pos[i]
+            val = speed[i]
+            cMap[key] = val
+        # print(cMap) {10: 2, 8: 4, 0: 1, 5: 1, 3: 3}
+        sortCars = sorted(cMap.items(), reverse = True)
+        # print(sortCars)
+        
+        maxTime = 0
+        
+        for p, s in sortCars:
+            reachTime = (target - p) / s
+            
+            if reachTime > maxTime:
+                res += 1
+                maxTime = reachTime
+        return res
