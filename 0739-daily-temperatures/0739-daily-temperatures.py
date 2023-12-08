badda81f94b00fcc3eaa88880f67a43a4a723855
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-08 14:58:26 -0500

    Time: 1160 ms (8.33%), Space: 31.6 MB (28.90%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,33 +1,35 @@
 class Solution:
     def dailyTemperatures(self, temps: List[int]) -> List[int]:
-        """
-        
-        stack = [73, 74, 75, 71, 69, 72, 76 ]
-        maxStack = [73, 74, 75]
-        check if the next added temp to maxStack is greater or less than the current day temparature in the stack
-        if greater than 1 day
-        if not: count += 1
-        output = [1, 1, 4, ]"""
+        # Create an array to store the result (initialized with zeros)
         output = [0] * len(temps)
+        
+        # Create a stack to store temperature indices
         stack = []
+        
         stack.append((0, temps[0]))
         
+        # Iterate through the temperatures starting from the second day
         for i in range(1, len(temps)):
+            # Get the current temperature
             currTemp = temps[i]
-            # print("Current Temperature: ", temps[i])
+            
+            # Check if the stack is not empty
             while stack:
+                # Get the previous temperature and its index from the top of the stack
                 prevTemp = stack[-1][1]
                 prevIndex = stack[-1][0]
-                # print("Prev Temp & Index: ", stack[-1][1], "&", stack[-1][0])
+                
+                # Compare the current temperature with the previous temperature
                 if currTemp > prevTemp:
-                    # print("Stack: ", stack)
+                    # If the current temperature is higher, update the result
                     output[prevIndex] = i - prevIndex
-                    stack.pop()
-                    # print("i: ", i)
-                    # print("i - prevIndex = ", i - prevIndex)
-                    # print()
+                    stack.pop()  # Remove the previous index from the stack
                 else:
+                    # If the current temperature is not higher, break the loop
                     break
-             
+            
+            # Add the current temperature's index to the stack
             stack.append((i, temps[i]))
+        
+        # Return the resulting array
         return output
