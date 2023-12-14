7b8c13fbf3a7032ee5bebffc7333e16446cdd9b3
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-14 18:44:04 -0500

    Time: 143 ms (39.22%), Space: 17.8 MB (5.71%) - LeetHub

--- 0910-smallest-range-ii/0910-smallest-range-ii.py
+++ 0910-smallest-range-ii/0910-smallest-range-ii.py
@@ -1,26 +1,28 @@
 class Solution:
     def smallestRangeII(self, nums: List[int], k: int) -> int:
-        # Sort the list first to handle the sequence in an ordered manner
+        """
+        Goal: squeeze the range as much as possible to have a smaller range aka the difference between the maximum and minimum elements"""
+        # Sort the list
         nums.sort()
-
-        # Calculate the initial range of the array
+        # actual range of the array
         originalRange = nums[-1] - nums[0]
-        
         # Initialize the result with the original range
         result = originalRange
-
         # Iterate through the array
         for i in range(len(nums)):
             # When i is the last index, break the loop as we can't use nums[i + 1]
             if i == len(nums) - 1:
                 break
 
-            # Calculate the potential new min and max
+            # Maximize/Increase the minimum value
+            """nums [↑, ↓] minimize immediate next val"""
             newMin = min(nums[0] + k, nums[i + 1] - k)
             #nums[i] < nums[i + 1] that's why minimize the i + 1 value
+            """nums[.....,↑,...........,↓] increase the current i"""
+            #Minimize/ Decrease the maximum value
             newMax = max(nums[i] + k, nums[-1] - k)
 
-            # Calculate the new range and update the result if it's smaller
+            # new range and update the result if it's smaller
             result = min(result, newMax - newMin)
 
         return result
