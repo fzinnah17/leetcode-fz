9a1846fb6d630d394a663b0f77cc2cdb852fcd75
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-18 11:20:47 -0500

    Time: 126 ms (9.44%), Space: 24 MB (5.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,8 +1,22 @@
 class Solution:
-    def groupAnagrams(self, strs):
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        Arrays and Hashing: Store the times users have logged in inside an array and later retreive the recent logged in times
+        
+        For this problem,
+        in the array the strings are added.
+        I am retriving the strings in an array that are anagrams
+        
+        Pseudocode:
+        1. iterate over the array
+        2. count the frequency of each character of each string so e: 1, a: 1, t: 1 and have that inside the dictionary key = strs, val = frequency
+        3. sort the dictionary values
+        4. iterate over the dictionary values that are sorted
+        5. whichever counts are matching, we can just add combine those characters as a string and append inside a list
+        """
         result = {}
         for s in strs:
-            char_count = {}  # key = character, count = frequency
+            char_count = {} #key = character, count = frequncy
             for ch in s:
                 if ch not in char_count:
                     char_count[ch] = 1
@@ -18,3 +32,5 @@ class Solution:
         groupedAnagrams = list(result.values())
         
         return groupedAnagrams
+        
+        
