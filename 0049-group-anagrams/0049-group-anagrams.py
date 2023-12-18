70d14424b53e9f067c03cf3f210e05995181306c
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-18 11:29:00 -0500

    Time: 100 ms (56.08%), Space: 19.8 MB (66.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,36 +1,32 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         """
-        Arrays and Hashing: Store the times users have logged in inside an array and later retreive the recent logged in times
-        
-        For this problem,
-        in the array the strings are added.
-        I am retriving the strings in an array that are anagrams
-        
-        Pseudocode:
-        1. iterate over the array
-        2. count the frequency of each character of each string so e: 1, a: 1, t: 1 and have that inside the dictionary key = strs, val = frequency
-        3. sort the dictionary values
-        4. iterate over the dictionary values that are sorted
-        5. whichever counts are matching, we can just add combine those characters as a string and append inside a list
-        """
-        result = {}
-        for s in strs:
-            char_count = {} #key = character, count = frequncy
-            for ch in s:
-                if ch not in char_count:
-                    char_count[ch] = 1
-                else:
-                    char_count[ch] += 1
-            key = tuple(sorted(char_count.items()))
-            
-            if key not in result:
-                result[key] = [s]  # Initialize a list for this character count
+        1. Create an empty dictionary to store anagram groups.
+        2. For each word in the input:
+           a. Sort the words and join them back together
+           b. Check if it is already a key in map.
+               I. If not, create a new entry as the key and a list containing as the value.
+               II. If already in map, append `word` to the list of values associated with that key.
+        3. After processing all words, return a list containing the values (lists of words) from the dictionary.
+"""
+        sMap = {} #key: sorted strings value = actual string
+        for w in strs:
+            sortStrs = "". join(sorted(w))
+            # print(w)
+            # print(sortStrs)
+            # print()
+            # eat  aet
+            # tea  aet
+            # tan  ant
+            # ate  aet
+            # nat  ant
+            # bat  abt
+
+            if sortStrs not in sMap:
+                sMap[sortStrs] = [w]
             else:
-                result[key].append(s)
-            
-        groupedAnagrams = list(result.values())
-        
-        return groupedAnagrams
-        
+                sMap[sortStrs]. append(w)
+            # print(sMap)
+            # {'aet': ['eat', 'tea', 'ate'], 'ant': ['tan', 'nat'], 'abt': ['bat']}
+        return sMap.values()
         
