112533758b187bc200c39300cd501dba51bd51f9
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-17 21:01:35 -0500

    Time: 91 ms (89.13%), Space: 21.2 MB (34.36%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        nMap = defaultdict(int)
-        for e in nums:
-            if e not in nMap:
-                nMap[e] = 1
-            else:
-                nMap[e] += 1
-        # print(nMap)
-        topK = sorted(nMap, key = nMap.get, reverse = True)[:k]
-        # return 
-        # print(nMap)
-        # print()
-        # print(topK)
-        return topK
-
-        # res = list(nMap.keys())[:k]
-        # return res
+        numMap = {}
         
+        for i in range(len(nums)):
+            if nums[i] not in numMap:
+                numMap[nums[i]] = 1
+            else:
+                numMap[nums[i]] += 1
+        
+        # Sort the dictionary items by their values (frequencies) in descending order
+        sorted_items = sorted(numMap.items(), key=lambda x: x[1], reverse=True)
+        
+        # Extract the keys (elements) corresponding to the first k items
+        result = [item[0] for item in sorted_items[:k]]
+        
+        return result
