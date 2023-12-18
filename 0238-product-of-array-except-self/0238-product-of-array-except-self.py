14fa0b39df61afe17cd2008e06682cab6c51ddde
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-18 12:32:52 -0500

    Time: 198 ms (45.63%), Space: 25.7 MB (8.17%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,23 +1,46 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        """
-        1. Initialize the result array
-        2. Variables: Initialize the prefix and postfix
-        3. Multiply by prefix product
-        4. Multiply by postfix product
-        5. update the prefix product
-        6. update the postfix product
-        7. return the result array"""
-        n = len(nums)
-        res = [1] * n
         
-        preFix = 1
-        postFix = 1
+        """
+        Array and Hashing: Store all the data in a list and retrieve the data from the list
+        for this problem, it is giving us an array, we have to return an array that for each ith place we have the product of other nums
+        -> retrieve the products of others nums from the array
+        
+        Pseudocode:
+        in =  [ 1, 2,  3, 4]
+        out = [24, 12, 8, 6]
+        
+        regular prefix = [] -> [1] -> [1, 2] -> [1,2,3] -> [1,2,3, 4]
+        regular postfix = [1,2,3,4] -> [2,3,4] -> [3,4] -> [4] -> []
+        
+        multiplied prefix = [1] -> -> [1,1*2 = 2] -> [1,2, 1*1*2*3 = 6] -> [1,2,6, 1*1*2*3*4 = 24]  
+        -> [1,1,2,6,24]
+        multiplied postfix = [1*2*3*4 = 24] -> [2*3*4 = 24] -> [3*4 = 12] -> [4] -> []  
+        -> [24,24,12,4,1]
+        
+       [ 1,  1,  2,  6, 24]
+           x   x   x   x
+       [24, 24, 12, 4,   1]
+       
+       [24, 12, 8, 6]
+       
+       how to do this zigzag multiplication?
+       
+        """
+        n = len(nums)
+        preList = [1] * n
+        postList = [1] * n
+        
+        for i in range(1,n):
+            preList[i] = preList[i - 1] * nums[i - 1]
+        
+        for i in range(n - 2, -1, -1):
+            postList[i] = postList[i+1] * nums[i + 1]
+        
+        result = []
         
         for i in range(n):
-            res[i] *= preFix
-            res[n-1-i] *= postFix
+            result.append(preList[i] * postList[i])
+        
+        return result
             
-            preFix *= nums[i]
-            postFix *= nums[n - 1 - i]
-        return res
