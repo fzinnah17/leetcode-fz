6a1f90bd3f6ea6170b543b5559dc21063bba4831
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-19 09:19:21 -0500

    Time: 100 ms (66.96%), Space: 16.3 MB (36.18%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,45 +1,63 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rowSet = defaultdict(set) #()
-        colSet = defaultdict(set) #()
-        squareSet = defaultdict(set) #()
+        """
+        Concept:
+        1. Arrays and Hashing: Storing data in an array and retrieving data from the array
+        2. Matrix: Implementing complex data grids or spreadsheet-like interfaces. Visualizes multi-dimensional data.
         
-        #functions are for debugging processes -> easily identify what's wrong
-        #check from the board:
+        For this problem, 
+        a. Row data: Digits 1-9 without repetition. So, store/assign the missing data
+        b. Column data: Digits 1-9 without repetition. So, store/assign the missing data
+        c. Similar for the 3x3 sub-boxes
+        d. Storing Data: The code uses sets (rowVisited, colVisited, squareVisited) to store data (digits) for each row, column, and 3x3 subgrid. These sets are used to keep track of digits to ensure there are no duplicates.
+        e. Retrieving Data: The code retrieves data from the Sudoku board (board[r][c]) to check the validity of each cell. It checks whether a digit is already present in the corresponding sets to validate the Sudoku board.
+        f. Matrix: The Sudoku board is represented as a matrix (board), and the code traverses this matrix to validate the Sudoku puzzle.
         """
-        breaking the logic down
-            a. function(params):
-                    same logic but specific r,c,s
-                        False
-                    True
-        not function(params) -> False
-        """
-        #check row
-        def checkRow(r,num):
-            if num in rowSet[r]:
+        Row = len(board)
+        Col = len(board[0])
+        
+        rowVisited = []
+        for _ in range(Row):
+            rowVisited.append(set())
+
+        colVisited = []
+        for _ in range(Col):
+            colVisited.append(set())
+
+        squareVisited = []
+        for _ in range(9):
+            squareVisited.append(set())
+
+        
+        def checkRow(row, num):
+            if num in rowVisited[row]:
+                return False
+            return True
+        
+        def checkCol(col, num):
+            if num in colVisited[col]:
                 return False
             return True
 
-        #check col
-        def checkCol(c,num):
-            if num in colSet[c]:
+        def checkSquare(row, col, num):
+            square = (row // 3) * 3 + col // 3
+            if num in squareVisited[square]:
                 return False
             return True
-        
-        #check box
-        def checkSquare(r,c,num):
-            if num in squareSet[(r//3,c//3)]:
-                return False
-            return True
-            
-        for r in range(len(board)):
-            for c in range(len(board)):
-                if board[r][c] == ".":
+
+        for r in range(Row):
+            for c in range(Col):
+                if board[r][c] == ".": # Because we don't fill empty spaces in this question
                     continue
-                if not(checkRow(r,board[r][c]) and checkCol(c,board[r][c]) and checkSquare(r,c,board[r][c])):
+                # Edge cases
+                if not (
+                    checkRow(r, board[r][c]) and
+                    checkCol(c, board[r][c]) and
+                    checkSquare(r, c, board[r][c])
+                ):
                     return False
-                rowSet[r].add(board[r][c])
-                colSet[c].add(board[r][c])
-                squareSet[(r//3,c//3)].add(board[r][c])
+                rowVisited[r].add(board[r][c])
+                colVisited[c].add(board[r][c])
+                square = (r // 3) * 3 + c // 3
+                squareVisited[square].add(board[r][c])
         return True
-        
