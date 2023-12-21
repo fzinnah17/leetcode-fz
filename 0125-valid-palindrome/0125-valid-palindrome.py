6bc51bf9b821f57fbbffabd3e8a17755ba6fb16a
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-20 22:05:39 -0500

    Time: 54 ms (40.97%), Space: 17.8 MB (23.88%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,40 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        """
+        Two pointers concept is used in: 
+        a. Optimizing algorithms for processing large files or streams, like handling large JSON responses in a REST API.
+        b. Optimizing memory usage in file uploads or streaming services.
+        c. For example, when reading and parsing a large log file, you can use two pointers to keep track of the current position and process the file in chunks, reducing memory usage and improving performance.
+        d.  Instead of loading the entire file into memory, you can use two pointers to read and process the file in smaller segments, reducing the memory footprint of your application.
+        
+        For this problem, it is used as: efficient character comparison within a string.  It is keeping track of the current position on both sides and processing it in chunks, reducing the memory usage and improving performance.
+        
+        Pseducode:
+        1. left begin, right end
+        2. check if each letters are same, 
+            if so, increment both pointers
+            if any of those is " "space, then increment that pointer/skip that position
+            return true if all the characters match
+        3. if not found, false
+        """
         s = s.lower()
-        def helper(c):
-            return c.isalpha() or c.isdigit()
-        left = 0
-        right = len(s) - 1
+        
+        left, right = 0, len(s) - 1
+        
+        def helper(i, direction):
+            while 0 <= i < len(s) and not s[i].isalnum():
+                i += direction
+            return i
         
         while left < right:
-            while left < right and not helper(s[left]):
-                left += 1
-            while left < right and not helper(s[right]):
-                right -= 1
+            left = helper(left, 1)
+            right = helper(right, -1)
             
-            if s[left] != s[right]:
+            if left < right and s[left] != s[right]:
                 return False
+            
             left += 1
             right -= 1
+        
         return True
+        
