564db48f4e89e5cdc7a2daed5c9aa402a78894b7
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-25 23:19:38 -0500

    Time: 753 ms (64.09%), Space: 39.5 MB (23.03%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,32 +1,10 @@
 class Solution:
-    def carFleet(self, target: int, pos: List[int], speed: List[int]) -> int:
-        """
-        {
-        10 : 2
-        8 : 4
-        0 : 1
-        5 : 1
-        3 : 3}
-    check if k + v is same to any other k + v, then fleet += 1
-    even if not fleet += 1 as car itself is a fleet
-        """
-        cMap = {}
-        res = 0
-        
-        for i in range(len(pos)):
-            key = pos[i]
-            val = speed[i]
-            cMap[key] = val
-        # print(cMap) {10: 2, 8: 4, 0: 1, 5: 1, 3: 3}
-        sortCars = sorted(cMap.items(), reverse = True)
-        # print(sortCars)
-        
-        maxTime = 0
-        
-        for p, s in sortCars:
-            reachTime = (target - p) / s
-            
-            if reachTime > maxTime:
-                res += 1
-                maxTime = reachTime
-        return res
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
