ad1083b193a1064f7ee1f35122dbd1542548a9af
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-27 14:20:46 -0500

    Time: 385 ms (5.75%), Space: 18.7 MB (7.46%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,19 +1,17 @@
 class Solution:
-    """
-    TC: O(logn)
-    SC: O(1)
-    """
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            middle = left + ((right - left) // 2)
-            
-            if nums[middle] < target:
-                left = middle + 1
-            elif nums[middle] > target:
-                right = middle - 1
-            else:
-                return middle
-        return -1
-            
+        nums = sorted(nums)  # Sorting the array is not necessary for a binary search.
         
+        l = 0
+        r = len(nums) - 1
+        
+        while l <= r:  # Change the condition to include equality.
+            mid = l + ((r - l) // 2)
+            
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:  # Adjust the comparisons here.
+                r = mid - 1
+            else:
+                l = mid + 1
+        return -1
