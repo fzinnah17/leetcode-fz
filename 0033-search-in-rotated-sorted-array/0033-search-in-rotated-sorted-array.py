52de45b55dd57fbeff4cd089e4bd5de56c970765
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-31 13:53:31 -0500

    Time: 43 ms (81.95%), Space: 17.5 MB (16.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,27 +1,48 @@
-class Solution(object):
-    def search(self, nums, target):
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
+        TC: O(logn) for dividing the search space in half with each iteration of the loop
+        SC: O(1)
+        It all depends on mid value so 
+            if left val < mid
+                left half?
+                right half?
+            if left val > mid
+                right half?
+                left half?
+        
         """
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            middle = left + ((right - left) // 2)
-            
-            if nums[middle] >= nums[left]:
-                if target > nums[middle] or target < nums[left]:
-                    left = middle + 1
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            m = l + ((r - l) // 2)
+
+            # Check if the middle element is the target
+            # nums = [1, 2, 3, 4, 5]
+            # target = 3
+            # Middle value m = 2
+            if nums[m] == target:
+                return m
+
+            # If the left side is sorted from min val
+            # nums = [4, 5, 6, 7, 0, 1, 2]
+            # target = 5
+            # Middle value index m = 3, value = 7
+            if nums[l] <= nums[m]:
+                # Check if the target is in the left half
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                # if target = 1
                 else:
-                    right = middle - 1
+                    l = m + 1
+            # If the right side is sorted from min val
+            #if nums[l] > nums[m]
+            #Example: nums = [6, 7, 8, 1, 2, 3, 4, 5], target = 2, middle index = 3(value = 1)
             else:
-                if target < nums[middle] or target > nums[right]:
-                    right = middle - 1
+                # Check if the target is in the right half target = 3
+                if nums[m] < target <= nums[r]:
+                    l = m + 1
+                #target = 8
                 else:
-                    left = middle + 1
-            if nums[middle] == target:
-                return middle
-        return - 1
-                
-        
-        
+                    r = m - 1
+        return -1
