525eafef8b93d9406b0c389fb4d929f0fa134696
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-01-02 23:20:36 -0500

    Time: 1694 ms (5.01%), Space: 28.3 MB (10.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,31 +1,19 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        So, the most naive way to approach this would be to choose each element as a starting position using a for loop, then using another forloop, we iterate through and record the maximum profit.
-
-This approach does the job, but it takes O(N^2) runtime.
-
-This means that we will slide the window by one element when we move on to the next array.
-1. two pointers: left, right
-2. maxProfit
-3. left > right: right += 1
-    right > left: max= max (itself, right - left)
-    left += 1
-
-        """
-        maxProfit = 0
-        buy, sell = 0, 1
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Initialize variables to keep track of minimum purchase price and maximum profit
+        min_purchase = float("inf")  # Set to positive infinity
+        max_profit = 0
         
-        while sell < len(prices):
-            if prices[sell] > prices[buy]:
-                maxProfit = max(maxProfit, (prices[sell] - prices[buy]))
-            else:
-                buy = sell
-            sell += 1
-        return maxProfit
-
-                
+        # Iterate through the prices list
+        for price in prices:
+            # Update the minimum purchase price if the current price is lower
+            min_purchase = min(min_purchase, price)
             
-        
+            # Calculate the potential profit by selling at the current price
+            potential_profit = price - min_purchase
+            
+            # Update the maximum profit if the potential profit is greater
+            max_profit = max(max_profit, potential_profit)
+            
+        # Return the maximum profit obtained
+        return max_profit
