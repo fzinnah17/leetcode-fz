1e5ed275d50bf70066985ca78229c8be5f65a4e8
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-01-07 16:19:20 -0500

    Time: 28 ms (97.20%), Space: 17.4 MB (16.29%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,18 +2,7 @@ class Solution(object):
     @lru_cache()
     def uniquePaths(self, m, n):
         """
-        1. Identify the Recursive Function:
-        2. Define a Memoization Table: {} I am trying to save the paths that has already been visited earlier so that path is not traversed again.
-        3. Implement the Base Case(s):
-                a. if m = 2 and n = 2:
-                    return 2 (as there are 2 ways to reach the star)
-                b. initial position: grid[0][0] return 1
-                c. when it reaches the destination: grid[m-1][n-1] return 1
-        4. Check the Memoization Table: If a cached result exists, return it immediately.
-        5. Perform Recursive Calls with the the Recursive Function: there's only choices of going down and right only. So does it mean we add up the previous two calls?
-        6. Cache and Return the Result: Store the computed result in the memoization table for future use.
-        TC: O(m * n) because for each position (m, n), we make two recursive calls: one for (m - 1, n) and one for (m, n - 1).
-        SC: O(m + n) for the memoTable
+
         """
         memoTable = {}
         if m == 1 and n == 1:
