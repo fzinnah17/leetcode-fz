d2991edb1f003f4ce93fff3f1325625e92f200de
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-01-08 21:50:51 -0500

    Time: 89 ms (87.37%), Space: 22 MB (22.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,23 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        numsMap = {} #key = nums[i], value: count
+        numsMap = {}
         
         for i in range(len(nums)):
             if nums[i] not in numsMap:
                 numsMap[nums[i]] = 1
             else:
                 numsMap[nums[i]] += 1
-        # print(numsMap) 
-        # print()
+        
         sortedMap = sorted(numsMap.items(), key = lambda x : x[1], reverse = True)
-        """
-        Now I want to return the 'k' keys of the tuple"""
-        # print(sortedMap[:k])
-        # print()
-        res = [n[0] for n in sortedMap[:k]]
-        # print(res)
-        return res
-        """
-        Time: O(n) to create the map, sort the map is O(logn), O(n) to find the first k time repeated items = O(nlogn)
-        Space: O(n) for creating a map
-        """
+        return [i[0] for i in sortedMap[:k]]
+        # print(numsMap)
+        
