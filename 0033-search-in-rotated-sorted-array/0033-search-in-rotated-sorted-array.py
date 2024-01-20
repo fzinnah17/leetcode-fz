2a3104f1f0b0ce51c9e2996bde7370b4c983f353
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-01-19 22:47:41 -0500

    Time: 46 ms (62.29%), Space: 17.1 MB (54.51%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,48 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         """
-        TC: O(logn) for dividing the search space in half with each iteration of the loop
-        SC: O(1)
-        It all depends on mid value so 
-            if left val < mid
-                left half?
-                right half?
-            if left val > mid
-                right half?
-                left half?
-        
+        nums = [4, 5, 6, 7, 0, 1, 2],  target = 5
+        nums = [6, 7, 8, 1, 2, 3, 4, 5], target = 2
+        choose only one side which is left and then inside that loop work with the boundaries
         """
         l, r = 0, len(nums) - 1
         
         while l <= r:
             m = l + ((r - l) // 2)
-
-            # Check if the middle element is the target
-            # nums = [1, 2, 3, 4, 5]
-            # target = 3
-            # Middle value m = 2
+            # nums = [4, 5, 6,   7,   0, 1, 2],  mid 7, target = 5
             if nums[m] == target:
                 return m
-
-            # If the left side is sorted from min val
-            # nums = [4, 5, 6, 7, 0, 1, 2]
-            # target = 5
-            # Middle value index m = 3, value = 7
-            if nums[l] <= nums[m]:
-                # Check if the target is in the left half
-                if nums[l] <= target < nums[m]:
+            elif nums[m] >= nums[l]:
+                if nums[m] > target >= nums[l]:
+                #answer is in the left portion of the array
                     r = m - 1
-                # if target = 1
                 else:
                     l = m + 1
-            # If the right side is sorted from min val
-            #if nums[l] > nums[m]
-            #Example: nums = [6, 7, 8, 1, 2, 3, 4, 5], target = 2, middle index = 3(value = 1)
+            #if nums[m] < nums[l]:
             else:
-                # Check if the target is in the right half target = 3
                 if nums[m] < target <= nums[r]:
+                #answer is in the right portion of the array
                     l = m + 1
-                #target = 8
                 else:
                     r = m - 1
         return -1
