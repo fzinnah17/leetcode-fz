6af7aa30caa6613befe1614ccf7a92511c80cc23
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-01-19 21:47:17 -0500

    Time: 41 ms (84.09%), Space: 16.8 MB (56.20%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -8,10 +8,10 @@ class Solution:
         while l <= r:
             m = l + ((r - l) // 2)
             
-            #if middle > nums left nums = [4,5,6, 7 ,0,1,2] answer is in the right side of the array
+            #if middle > nums right nums = [4,5,6, 7 ,0,1,2] answer is in the right side of the array
             if nums[m] > nums[r]:
                 l = m + 1
-            #if middle < nums left nums = [6, 7, 8,   1,   2, 3, 4, 5] so answer is on the right side
+            #if middle < nums right nums = [6, 7, 8,   1,   2, 3, 4, 5] so answer is on the other side
             elif nums[m] < nums[r]:
                 r = m 
             else:
