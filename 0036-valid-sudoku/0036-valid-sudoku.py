934466f79bca00cf6ad923fbba843e609afbc75c
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-01-24 16:29:20 -0500

    Time: 89 ms (86.37%), Space: 16.7 MB (59.05%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,22 +1,30 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        Row = len(board)
-        Col = len(board[0])
         
-        visited = defaultdict(dict)
+        ROW, COL = len(board), len(board[0])
+        row, col, grid = defaultdict(list), defaultdict(list), defaultdict(list)
+        
+        # row = {index:[values]}
+        # col = ()
+        # grid = ()
+                
+        
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                
+                if board[r][c] == ".": continue
 
-        def checkValid(r, c, num):
-            if num in visited['row'].get(r, set()) or num in visited['col'].get(c, set()) or num in visited['square'].get((r // 3, c // 3), set()):
-                return False
-            return True
-
-        for r in range(Row):
-            for c in range(Col):
-                if board[r][c] == ".":
-                    continue
-                if not checkValid(r, c, board[r][c]):
+                # check if seen
+                if board[r][c] in row[r] or board[r][c] in col[c] or board[r][c] in grid[(r // 3, c // 3)]:
                     return False
-                visited['row'].setdefault(r, set()).add(board[r][c])
-                visited['col'].setdefault(c, set()).add(board[r][c])
-                visited['square'].setdefault((r // 3, c // 3), set()).add(board[r][c])
+                
+                # add new char
+                row[r].append(board[r][c])
+                col[c].append(board[r][c])
+                grid[(r // 3, c // 3)].append(board[r][c])
+                
         return True
+            
+            
+        
+        
