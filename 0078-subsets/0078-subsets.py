38b55d8cdcabe5913c922b63613a7964f5edaaeb
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-01-24 12:09:02 -0500

    Time: 48 ms (17.43%), Space: 16.7 MB (63.97%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,27 @@
-class Solution(object):
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        result = []
-        def helper(start, curr):
-            result.append(curr[:])
-            for i in range(start, len(nums)):
-                curr.append(nums[i])
-                helper(i + 1, curr)
-                curr.pop()
-        helper(0,[])  
-        return result
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+      """
+      [2,3,4,5]
+      first ques how many subsets will be there? 2^n
+      output = [[], [2], [3], [4], [5], [2,3], [2,4], [2,5], [2,3,4], [2,3,5], [2,4,5], [2,3,4,5], [3,4], [3,5], [3,4,5], [4,5]]
+      """
+      res = []
+      
+      subset = [] #allocate an array that can give global access to the helper function
+      
+      #i is the index of the value we are making the decision on
+      def dfs(i):
+        #if we finished iterating the array then we know we can add it to the result list
+        if i >= len(nums):
+          res.append(subset[:]) #[:] or copy() because subset will be modified in each level of the decision tree
+          return
         
+        #we choose the path
+        subset.append(nums[i])
+        dfs(i + 1) #this has the new element we are adding
+        
+        #we do not choose the path means 
+        subset.pop() #this is for the empty subset
+        dfs(i + 1)
+      dfs(0)
+      return res
