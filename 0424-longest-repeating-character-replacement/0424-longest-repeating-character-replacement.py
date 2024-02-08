92647972c5b025d86680f4fb668b4abdc57223c0
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-02-08 11:37:41 -0500

    Time: 92 ms (63.85%), Space: 16.6 MB (83.76%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,23 +1,28 @@
+from collections import defaultdict
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        sMap = {}
-        longest = 0
-        max_count = 0
+        """
+        count how many times each character is present using defaultdict not counter
+        have pointers
+        if l != r: and as long as k exists and != 0: replace the character with the highest character and
+        calculate the length
+        return the maximum length
+        """
+        strDict = defaultdict(int)
         l, r = 0, 0
+        maxFreq = 0
+        output = 0
         
         while r < len(s):
-            if s[r] not in sMap:
-                sMap[s[r]] = 1
-            else:
-                sMap[s[r]] += 1
-            max_count = max(max_count, sMap[s[r]])
-
-            if (r - l + 1 - max_count) > k:
-                sMap[s[l]] -= 1
+            strDict[s[r]] += 1
+            maxFreq = max(maxFreq, strDict[s[r]])
+            
+            if r - l + 1 - maxFreq > k:
+                strDict[s[l]] -= 1
                 l += 1
-
-            longest = max(longest, r - l + 1)
-
-            r += 1  # Increment the right pointer
-
-        return longest
+            
+            output = max(output, r - l + 1)
+            r += 1
+        
+        return output
