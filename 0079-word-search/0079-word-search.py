34f6616e7e04058aa293fe873eae437d38efd6f8
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-02-09 17:32:52 -0500

    Time: 4294 ms (55.76%), Space: 16.5 MB (89.88%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,36 +1,38 @@
 class Solution:
-    def exist(self, grid: List[List[str]], word: str) -> bool:
-        Rows = len(grid)
-        Cols = len(grid[0])
-        visited = set()
-
-        # Define a recursive depth-first search (DFS) function.
-        def dfs(r, c, pointer):
-            # Check if we've matched the entire word.
-            if len(word) == pointer:
-                return True
-            # Check for boundary conditions and visited cells.
-            if (r < 0 or c < 0
-                or r >= Rows or c >= Cols
-                or (r, c) in visited
-                or grid[r][c] != word[pointer]):
-                return
-
-            visited.add((r, c))
-            # Recursively search in all four directions.
-            res = (dfs(r + 1, c, pointer + 1) or
-                   dfs(r - 1, c, pointer + 1) or
-                   dfs(r, c + 1, pointer + 1) or
-                   dfs(r, c - 1, pointer + 1))
-            visited.remove((r, c)) #backtracking
-            return res
-
-        # Iterate through the grid to find the starting point for the word.
-        for row in range(Rows):
-            for col in range(Cols):
-                if grid[row][col] == word[0]:
-                    # Start DFS from this point.
-                    if dfs(row, col, 0):
-                        return True
-        # If we didn't find the word starting from any point, return False.
-        return False
+    def exist(self, board: List[List[str]], word: str) -> bool:
+      
+      Row = len(board)
+      Col = len(board[0])
+      visited = set()
+      
+      def dfs(r, c, pointer):
+        
+        #base cases:
+        
+        if len(word) == pointer:
+          return True
+        
+        if (r < 0 or 
+            c < 0 or 
+            r >= Row or 
+            c >= Col or 
+            (r, c) in visited or 
+            board[r][c] != word[pointer]):
+          return
+        
+        visited.add((r, c))
+        res = (dfs(r + 1, c, pointer + 1) or 
+               dfs(r - 1, c, pointer + 1) or 
+               dfs(r, c + 1, pointer + 1) or 
+               dfs(r, c - 1, pointer + 1))
+        visited.remove((r,c))
+        return res
+      
+      for row in range(Row):
+        for col in range(Col):
+          # if the first character matches then call the dfs function -> condition
+          if board[row][col] == word[0]:
+            if dfs(row, col, 0):
+              return True
+      return False
+        
