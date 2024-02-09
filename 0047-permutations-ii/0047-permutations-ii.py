4a0be7c714fbf1030ce684eda8e29d2f3342d0e0
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-02-09 11:48:27 -0500

    Time: 436 ms (12.65%), Space: 17 MB (57.80%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -3,12 +3,14 @@ class Solution:
       if len(nums) == 1:
         return [[nums[0]]]
       
-      res = set()
+      res = set() #a list is mutable and cannot be added to a set. we first convert the list to a tuple using the tuple() function, and then add it to the set.
       
       def dfs(path, remNums):
         
         #all the permuations are found and we reached the last element
         if not remNums:
+          print(f"Path as list: {path}")
+          print(f"Path as tuple before adding to res: {tuple(path)}")
           res.add(tuple(path))
           return
       
