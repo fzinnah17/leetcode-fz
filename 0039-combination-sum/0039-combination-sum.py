0d681ade5a2a4c46f27529955749e511b55f800b
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-02-15 12:28:31 -0500

    Time: 58 ms (43.15%), Space: 16.8 MB (57.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,32 +1,27 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-      """
-      decision tree:
-        1. take the target - curr num
-        7 - 2 = 5
-        2. check if the subtraction is there or not
-        3. if not then either use the same value or go to the next value
-             2                  3              6             7
-         /  /  \  \            / \            / \           / \
-         
-          """
+    def combinationSum(self, cans: List[int], target: int) -> List[List[int]]:
       res = []
-      combinations = []
-
+      combs = []
+      
       def dfs(i, curr):
+        
         if curr == target:
-          res.append(combinations[:])
+          res.append(combs[:])
           return
-        if i >= len(candidates) or curr > target:
+        
+        if curr > target or i >= len(cans):
           return
-
-          # Choose the current number and continue with the same number
-        combinations.append(candidates[i])
-        dfs(i, curr + candidates[i])
-        combinations.pop()  # Remove the last number
-
-        # Skip the current number and move to the next one
+        
+        #to include
+        combs.append(cans[i])
+        dfs(i, curr + cans[i])
+        
+        #to not include
+        combs.pop()
         dfs(i + 1, curr)
-
+        
       dfs(0, 0)
+      
       return res
+      
+        
