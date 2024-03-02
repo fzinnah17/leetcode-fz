a88e8f378890851ed76702277676de3f3c7ee780
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-02 14:41:33 -0500

    Time: 39 ms (44.21%), Space: 16.5 MB (94.46%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -3,30 +3,41 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # Create a dummy node to handle edge cases like removing the first node
-        dummy = ListNode(0)
-        dummy.next = head
-
-        # First pass: find the length of the list
-        length = 0
-        temp = head
-        while temp:
-            length += 1
-            temp = temp.next
-
-        # Calculate the position from the start (length - n)
-        length -= n
-
-        # Second pass: reach the node just before the target
-        temp = dummy
-        while length > 0:
-            length -= 1
-            temp = temp.next
-
-        # Remove the nth node from end
-        temp.next = temp.next.next
-
-        return dummy.next
+    def removeNthFromEnd(self, l: Optional[ListNode], n: int) -> Optional[ListNode]:
+      """
+      1. can n be larger than the linked list?
+      2. is it singly or doubly linked list?
+      3. will the loop have any cycle?
+      4. does n start from 0 or 1? it's 1 bc it's from the end of the list
+      
+      - come up with testcases:
+        [1,2,3], n = 1 return [1,2]
+        [1,2,3,4], n = 4 return [2,3,4]
+      
+      - can we use multiple passes? temple head? two pointers?
+        - multiple passes: first get length of the list, that's how we know the nth node from the end of the list
+        - one pass: two pointer strategy can help us look ahead
+        - temp head: we can point to the head
+      """
+      
+      temp = ListNode(0)
+      
+      slow = temp
+      slow.next = l
+      
+      fast = temp
+      
+      for i in range(n):
+        fast = fast.next
+        
+      #we don't know how many times so while loop
+      
+      while fast.next:
+        
+        fast = fast.next
+        slow = slow.next
+        
+      slow.next = slow.next.next
+      return temp.next
+        
