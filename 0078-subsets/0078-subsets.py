7688dd4c561032611bb17c5ecd6d1c7fe53e474c
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-03 13:55:50 -0500

    Time: 31 ms (90.90%), Space: 16.8 MB (50.73%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,25 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-      if len(nums) == 1:
-        return [[], [nums[0]]]
-      
-      res = []
-      subset = []
-      
-      def dfs(i):
-        #base case
-        if i == len(nums):
-          res.append(subset[:]) #subset's copy is added
-          return
+        """
+        to include
+        to not include
+        """
+        res = []
+        subs = []
         
-        #to include the number
-        subset.append(nums[i])
-        dfs(i + 1)
-        
-        # or to not include the number
-        subset.pop()
-        dfs(i + 1)
-        
-      dfs(0)
+        def dfs(i):
+          
+          if i == len(nums):
+            res.append(subs[:])
+            return
+          
+          subs.append(nums[i])
+          dfs(i+1)
+          subs.pop()
+          dfs(i+1)
+        dfs(0)
+        return res
       
-      return res
