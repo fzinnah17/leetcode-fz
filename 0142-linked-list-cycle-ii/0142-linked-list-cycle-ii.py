dd1e79136e6ddcdcd30a2c9054cc5cac7c471b35
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-04 12:03:24 -0500

    Time: 34 ms (96.60%), Space: 18.8 MB (97.77%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -1,53 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def detectCycle(self, head):
-        """
-        1. Identify the problem: problem statement indicating the presence of a cycle
-        2. Handle edge cases: linked list or array that is empty or a structure that doesn't have any cycles.
-        if head is None:
-            return None
-        3. Initialize the pointers:
-                a. fast = head,
-                b. slow = head
-        4. Use while loops with the given problem condition:
-            a. To move the pointers until you reach the end of the data structure or when you find a cycle.
-        while fast and fast.next:
-        5. Choose a movement strategy:
-            a. Tortoise and Hare: The fast pointer (the hare) moves faster than the slow pointer (the turtle).
-        fast = fast.next.next
-        slow = slow.next
-        6. Find cycles or repeating parts: This could happen when the fast pointer catches up to the slow pointer or when they both end up at the same element.
-        if fast == slow:
-        7. Time and space complexity: time complexity of O(N) and a space complexity of O(1)   
-                """
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
         slow = fast = head
-
-        if not head or not head.next:
-            return None
+        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+            #cycle found
             if slow == fast:
                 break
         else:
-            return None
-            
-        pointer = head
-        while pointer != fast:
-            pointer = pointer.next
-            fast = fast.next
-        return pointer
-                
-                
-                
-                
-            
-            
+            return
         
-        
+        temp = head
+        while temp != slow:
+            temp = temp.next
+            slow = slow.next
+        #as soon as temp and slow meets
+        return temp
         
