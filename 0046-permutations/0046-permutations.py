ac3a3c29660c5440a04e517d290df6631cb08ff9
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-05 22:54:18 -0500

    Time: 39 ms (69.99%), Space: 16.7 MB (51.89%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,31 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-      """
-      we have an array
-      pop one element
-      do the dfs on it
-      append it from the end
-      and do the dfs again
-      repeat the process until the last element of the array is reached
-      """
-      res = []
-      
-      #base case:
-      if len(nums) == 1:
-        return [[nums[0]]]
-      
-      def dfs(path, remNums):
-        # if no elements left from the nums array then just add it to the result
-        if not remNums:
-          res.append(path)
-          return
         
-        # we iterate over each of the array on each step to remove and add it back
-        for _ in range(len(remNums)):
-          el = remNums.pop(0)
-          dfs(path + [el], remNums)
-          remNums.append(el)
-
-      dfs([], nums)
-      
-      return res
+        res = []        
+        def dfs(path, remaining):
+            if not remaining:
+                res.append(path)
+                return
+            
+            for _ in range(len(remaining)):
+                element = remaining.pop(0)
+                dfs(path + [element], remaining)
+                remaining.append(element)
+            
+        dfs([], nums)
+        return res
+        
+        
