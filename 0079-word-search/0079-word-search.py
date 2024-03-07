091fc54ad3d0b25feead67c4713772c13a409fb2
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-07 13:50:38 -0500

    Time: 4560 ms (38.52%), Space: 16.6 MB (77.60%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,38 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-      
-      Row = len(board)
-      Col = len(board[0])
-      visited = set()
-      
-      def dfs(r, c, pointer):
+        visited = set()
         
-        #base cases:
+        def dfs(r,c,pointer):
+            if len(word) == pointer:
+                return True
+            
+            if r < 0 or c < 0 or r >= len(board) or c >= len(board[0]) or (r,c) in visited or board[r][c] != word[pointer]:
+                return False
+            
+            visited.add((r,c))
+            
+            res = (
+                dfs(r + 1, c, pointer + 1) or
+                dfs(r - 1, c, pointer + 1) or
+                dfs(r, c + 1, pointer + 1) or
+                dfs(r, c - 1, pointer + 1)
+            )
+            
+            visited.remove((r,c))
+            return res
         
-        if len(word) == pointer:
-          return True
-        
-        if (r < 0 or 
-            c < 0 or 
-            r >= Row or 
-            c >= Col or 
-            (r, c) in visited or 
-            board[r][c] != word[pointer]):
-          return
-        
-        visited.add((r, c))
-        res = (dfs(r + 1, c, pointer + 1) or 
-               dfs(r - 1, c, pointer + 1) or 
-               dfs(r, c + 1, pointer + 1) or 
-               dfs(r, c - 1, pointer + 1))
-        visited.remove((r,c))
-        return res
-      
-      for row in range(Row):
-        for col in range(Col):
-          # if the first character matches then call the dfs function -> condition
-          if board[row][col] == word[0]:
-            if dfs(row, col, 0):
-              return True
-      return False
-        
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == word[0]:
+                    if dfs(row,col,0):
+                        return True
+        return False
