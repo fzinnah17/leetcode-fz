65a27eecd35a619d81d7e1bf873adcb464c37568
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-07 00:09:50 -0500

    Time: 73 ms (28.07%), Space: 16.7 MB (49.51%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,32 +1,26 @@
 class Solution:
-    def combinationSum2(self, cans: List[int], target: int) -> List[List[int]]:
-      
-      res = set()
-      combs = []
-      cans.sort()
-      # print(cans) [1, 1, 1, 1, 2, 2, 3, 4, 4, 5, 5]
-      
-      
-      def dfs(i, curr):
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = set()
+        combs = []
+        candidates.sort()
         
-        if curr == target:
-          res.add(tuple(combs))
-          return
+        def dfs(i, curr):
+            
+            if curr == target:
+                res.add(tuple(combs))
+                return
+            
+            if i >= len(candidates) or curr > target:
+                return
+            
+            for k in range(i, len(candidates)):
+                if k > i and candidates[k] == candidates[k - 1]:
+                    continue
+                combs.append(candidates[k])
+                dfs(k + 1, curr + candidates[k])
+            
+                combs.pop()            
+            
+        dfs(0, 0)
         
-        if i >= len(cans) or curr > target:
-          return
-        for k in range(i, len(cans)):
-          if k > i and cans[k] == cans[k-1]:
-            continue
-          combs.append(cans[k])
-          dfs(k + 1, curr+ cans[k])
-
-          combs.pop()
-          # dfs(k + 1, curr) No need for an extra dfs call here since the loop will continue to the next index
-
-        
-      dfs(0, 0)
-      
-      return res
-          
-        
+        return res
