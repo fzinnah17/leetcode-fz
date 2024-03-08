f4be72479b86fc603aae943b99d676bd3f6fa99f
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-08 18:00:03 -0500

    Time: 450 ms (91.50%), Space: 35.2 MB (48.76%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,39 +1,31 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-      
-      """
-      Time: O(2^n)"""
-      
-      res = []
-      partitions = []
-      
-      if len(s) == 1:
-        return [[s]]
-      
-      def palindrome(s):
-        l, r = 0, len(s) - 1
-        
-        while l < r:
-          if s[l] != s[r]:
-            return False
-          l += 1
-          r -= 1
-        return True
-      
-      def dfs(i):
-        
-        #base case
-        if i >= len(s):
-          res.append(partitions[:])
-          return
-        
-        for j in range(i, len(s)):
-          if palindrome(s[i: j + 1]):
-            partitions.append(s[i:j+1])
-            dfs(j + 1)
-            partitions.pop()
-        
-      dfs(0)
-      
-      return res
+        if len(s) == 1:
+            return [[s[0]]]
+        res = []
+        parts = []
+            
+        def isPalindrome(s, left, right):
+            #we will use a two pointers approach
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(parts[:])
+                return
+            for j in range(i, len(s)):
+               #call the helper function
+               if isPalindrome(s, i, j):
+                    parts.append((s[i:j + 1]))
+                    dfs(j + 1)
+
+                    #backtrack
+                    parts.pop()
+        dfs(0)
+        return res
         
