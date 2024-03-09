52036c0550a88b17c95b6e72d6c56da80f917d28
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-09 14:34:39 -0500

    Time: 42 ms (48.81%), Space: 16.6 MB (53.55%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,25 +1,23 @@
-class Solution(object):
-    def mySqrt(self, x):
-        """   
-When you encounter a problem like this, the first step is to think about the time and space complexities of the different possible solutions. In this case, a brute force solution would be to iterate through all numbers from 0 to x and check which one squared is closest to x. However, this solution has a time complexity of O(x) and is not efficient for large x.
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l,r = 0, x
+        # While left pointer is less than right pointer we have not exhausted the possible numbers
+        while l <= r:
+            # Get the mid point of the two pointers 
+            mid = (l + r) // 2
 
-Next, you can think about other algorithms that might be more efficient. In this case, binary search is a good candidate because the square root of a number x is always between 0 and x. You can use binary search to find the number n such that n^2 <= x < (n+1)^2. Once you have found n, you can return it as the square root of x.
-
-Using binary search has a time complexity of O(log x), which is much better than the brute force solution. Therefore, when you encounter a problem that involves finding a value within a range, binary search is often a good algorithm to consider.
-        """
-        if x == 0:
-            return 0
-        if x == 1:
-            return 1
-        left, right = 2, x
-        while left <= right:
-            middle = left + ((right - left) // 2)
-            if middle * middle == x: # 2 = sqrt(4) => 2^2 = 4
-                return middle
-            elif middle * middle > x: # 5 > sqrt(4) => 5^2 = 4
-                right = middle - 1
-            else:
-                left = middle + 1 # 1 < sqrt(4) => 1^2 = 4
-        return right
-    #TC: O(n) SC: O(1)
+            # Check if mid point is less than or greater than or equal to our target
+            if mid ** 2 == x:
+                # If mid point is equal to our target then return our mid point
+                return mid
             
+            elif mid ** 2 < x:
+                # If mid point is less than our target, then we move the left pointer up to mid point + 1, because everything left of the mid point would be even further away from our target
+                l = mid + 1
+            else:
+                # If mid point is greater than our target, then we move the right pointer down to mid point - 1, because everything to the right of mid point is invalid. 
+                r = mid - 1
+                
+        # Return the right pointer for the closes number to square for our target as it is the last remaining valid number.
+        return r
+        
