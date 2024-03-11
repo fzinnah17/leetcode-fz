1b8a209ef540a7dfab7e0b1e6cbff4552f42587e
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-10 23:09:14 -0400

    Time: 28 ms (95.60%), Space: 16.8 MB (41.28%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,22 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        """
-        to include
-        to not include
-        """
         res = []
         subs = []
         
         def dfs(i):
-          
-          if i == len(nums):
-            res.append(subs[:])
-            return
-          
-          subs.append(nums[i])
-          dfs(i+1)
-          subs.pop()
-          dfs(i+1)
+            if i >= len(nums):
+                res.append(subs[:])
+                return
+            
+            subs.append(nums[i])
+            dfs(i + 1)
+            
+            subs.pop()
+            dfs(i + 1)
+            
         dfs(0)
         return res
-      
+        
