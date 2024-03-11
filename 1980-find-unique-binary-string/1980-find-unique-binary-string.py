e239fdca99fde7743715301acbfa4852c4bdacc5
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-11 00:22:11 -0400

    Time: 37 ms (67.34%), Space: 16.8 MB (26.78%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,10 +1,12 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         # Use a set for faster lookup
+        # once a valid binary string (not in num_set) is found, stop the recursion.
+        # Don't explore further after finding a valid string.
         num_set = set(nums)
         res = ""
         curr_str = []
-
+        
         def dfs(i):
             nonlocal res
             if i == len(nums[0]):
@@ -13,20 +15,45 @@ class Solution:
                     res = candidate
                     return True
                 return False
-
-            # Try with '0'
-            curr_str.append('0')
-            if dfs(i + 1):
-                return True
-            curr_str.pop()
-
-            # Try with '1'
-            curr_str.append('1')
-            if dfs(i + 1):
-                return True
-            curr_str.pop()
-
+            # Only continue recursion if we haven't found a result   
+            if not res:
+                curr_str.append("0")
+                if dfs(i + 1):
+                    return True
+                curr_str.pop()
+            
+            if not res:
+                curr_str.append("1")
+                if dfs(i + 1):
+                    return True
+                curr_str.pop()
             return False
-
+            
         dfs(0)
         return res
+
+#         def dfs(i):
+#             nonlocal res
+#             if i == len(nums[0]):
+#                 candidate = ''.join(curr_str)
+#                 if candidate not in num_set:
+#                     res = candidate
+#                     return True
+#                 return False
+
+#             # Try with '0'
+#             curr_str.append('0')
+#             if dfs(i + 1):
+#                 return True
+#             curr_str.pop()
+
+#             # Try with '1'
+#             curr_str.append('1')
+#             if dfs(i + 1):
+#                 return True
+#             curr_str.pop()
+
+#             return False
+
+#         dfs(0)
+#         return res
