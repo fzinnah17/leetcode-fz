3dab94000740e8f25fe5ab8c8a6b34d0212a26b8
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-12 16:06:26 -0400

    Time: 27 ms (97.38%), Space: 16.7 MB (41.78%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,10 +1,38 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         res = ""
+        curr_str = []
+        num_set = set(nums)
         
-        i = 0
+        def dfs(i):
+            nonlocal res
+            if res:
+                return
+            
+            
+            if i == len(nums[0]):
+                candidate = ''.join(curr_str)
+                # print(f"checking candidate: {candidate}")
+                if candidate not in num_set:
+                    res = candidate
+                    return
+                
+            if res:
+                return
+            if i < len(nums[0]):     
+                curr_str.append("0")
+                dfs(i + 1)
+                curr_str.pop()
+
+                if res:
+                    return
+
+                curr_str.append("1")
+                dfs(i + 1)
+                curr_str.pop()
         
-        for bin in nums:
-            res += str(1 - int(bin[i]))
-            i += 1
+        dfs(0)
         return res
+        
+    
+    
