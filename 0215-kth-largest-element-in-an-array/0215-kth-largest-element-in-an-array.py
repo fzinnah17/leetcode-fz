dc37435ff54ffe4ca29ae6942cbb2770d89b087e
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-16 13:39:48 -0400

    Time: 485 ms (44.60%), Space: 29.8 MB (75.29%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,8 +1,20 @@
 class Solution:
+    #n >>>>>>>>>>> k, we are just only taking a look at the 5th largest number
+    #T: O(nlogk), S: O(k) -> because heap is only size k
+    
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # Heapify the num array, create new array with negative values for each num, because python only supports minimum heaps.
-        heap = [-num for num in nums]
-        heapq.heapify(heap)
-        for _ in range(1,k):
-            heapq.heappop(heap)
-        return neg(heap[0])
+        #start with nothing, here time is O(1)
+        heap = []
+        #min heap in python
+        heapq.heapify(heap) #T: O(1), S: O(1)
+        
+        #use heap as a buffer by pushing stuff into the heap only k items from the given item, 
+        #so that if it exceeds then it will be popping from the top(which is on the right side). Then it will make sure that the top item is always the largest element.
+        
+        for i in range(len(nums)): #T: n * log(k)
+            if i < k:
+                heapq.heappush(heap, nums[i]) #T: logn
+            elif nums[i] > heap[0]:
+                #pop the top of the heap and push the number in
+                heapq.heapreplace(heap, nums[i]) #same as popping then pushing, T: O(log2n)
+        return heap[0]
