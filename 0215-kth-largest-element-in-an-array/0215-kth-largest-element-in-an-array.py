5eac2a8a0c64e8ceb5fa9c832fafadaee8e0d2e9
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-15 22:45:11 -0400

    Time: 497 ms (28.24%), Space: 29.9 MB (39.03%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -3,10 +3,6 @@ class Solution:
         # Heapify the num array, create new array with negative values for each num, because python only supports minimum heaps.
         heap = [-num for num in nums]
         heapq.heapify(heap)
-
-        # Remove k items from heap
         for _ in range(1,k):
             heapq.heappop(heap)
-        
-        # Return the top of the heap.
         return neg(heap[0])
