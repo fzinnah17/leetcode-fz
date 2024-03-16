041b166d946abb2c7c05bc50b188040498b72891
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-16 14:34:40 -0400

    Time: 37 ms (77.96%), Space: 16.6 MB (63.04%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,13 +1,37 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        count = 0
-        while len(students) > count:
-            if students[0] == sandwiches[0]:
-                sandwiches.pop(0)
-                count = 0
+        """
+        #simulating to use stack and queue without using any of these
+        Plan:
+        1. Count 1's and 0's from student array
+        2. iterate through the sandwiches array[as stack has a limitation to move around, but students can be moved anywhere for being in the queue]
+            a. if current sandwich element is circle
+                i. If circle students > 0, decrement circle student count
+                ii. else: if it is == 0: break out the logic because idk
+            b. If current sandwich element is square:
+                i. If square student > 0, decement square student count
+                ii. Else break out of the logic
+        3. Return total left (1's + 0's) --> Unfed student
+        """
+        circle_students = 0
+        square_students = 0
+        
+        for i in range(len(students)):
+            if students[i] == 0:
+                circle_students += 1
             else:
-                students.append(students[0])
-                count+=1
-
-            students.pop(0)
-        return len(students)
+                square_students += 1
+        for j in range(len(sandwiches)):
+            if sandwiches[j] == 0:
+                if circle_students > 0:
+                    circle_students -= 1
+                else:
+                    break
+            else:
+                if square_students > 0:
+                    square_students -= 1
+                else:
+                    break
+        return square_students + circle_students
+    
+        
