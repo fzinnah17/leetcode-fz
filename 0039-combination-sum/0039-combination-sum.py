117c200e6424266df6d171c34a47af87ea2f2b44
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-18 01:40:08 -0400

    Time: 48 ms (78.75%), Space: 16.9 MB (9.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,27 +1,27 @@
 class Solution:
-    def combinationSum(self, cans: List[int], target: int) -> List[List[int]]:
-      res = []
-      combs = []
-      
-      def dfs(i, curr):
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        cans = []
         
-        if curr == target:
-          res.append(combs[:])
-          return
+        def dfs(i, curr):
+            if curr == target:
+                res.append(cans[:])
+                return
+            
+            if i >= len(nums) or curr > target:
+                return
+            
+            for j in range(i, len(nums)):
+                 
+                #to include
+                cans.append(nums[j])
+                dfs(j, curr + nums[j]) #to allow the same number be used multiple times
+                # dfs(j + 1, curr + nums[j]) only if we were only finding the target not what we are looking up
+                #to not include
+                cans.pop()
+                # dfs(j + 1, curr) do not use it because we can use same number multiple times
+                
+        dfs(0, 0)
+        return res
         
-        if curr > target or i >= len(cans):
-          return
-        
-        #to include
-        combs.append(cans[i])
-        dfs(i, curr + cans[i])
-        
-        #to not include
-        combs.pop()
-        dfs(i + 1, curr)
-        
-      dfs(0, 0)
-      
-      return res
-      
         
