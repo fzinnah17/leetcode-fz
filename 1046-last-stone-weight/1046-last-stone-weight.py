ead36bb9941769ece3d3f3ee47b059bf93525ff3
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-19 12:46:50 -0400

    Time: 35 ms (63.63%), Space: 16.5 MB (43.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,15 +1,23 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
 
-        def remove_largest():
-            index_of_largest = stones.index(max(stones))
-            # Remove largest stone
-            return stones.pop(index_of_largest)
+        # Make all the stones negative. We want to do this *in place*, to keep the
+        # space complexity of this algorithm at O(1). :-)
+        for i in range(len(stones)):
+            stones[i] *= -1
 
+        # Heapify all the stones.
+        heapq.heapify(stones)
+
+        # While there is more than one stone left, remove the two
+        # largest, smash them together, and insert the result
+        # back into the heap if it is non-zero.
         while len(stones) > 1:
-            stone_1 = remove_largest()
-            stone_2 = remove_largest()
+            stone_1 = heapq.heappop(stones)
+            stone_2 = heapq.heappop(stones)
             if stone_1 != stone_2:
-                stones.append(stone_1 - stone_2)
+                heapq.heappush(stones, stone_1 - stone_2)
 
-        return stones[0] if stones else 0
+        # Check if there is a stone left to return. Convert it back
+        # to positive.
+        return -heapq.heappop(stones) if stones else 0
