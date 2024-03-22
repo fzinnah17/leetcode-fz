6f061da2553d8cac78992b0b9574c3e6c140e27f
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-21 20:46:33 -0400

    Time: 124 ms (15.26%), Space: 19 MB (9.35%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,9 +1,10 @@
 class Solution:
     
+    #closure approach -> inner function has access to all the global variables
     
     def validPalindrome(self, s: str) -> bool:
-        
-        def is_palindrome(s, skips, left, right):
+        #this is a nested function that requires not passing the string
+        def is_palindrome(skips, left, right):
             if skips > 1:
                 return False
 
@@ -13,12 +14,12 @@ class Solution:
                     right -= 1
                 else:
                     skips += 1
-                    left_skip = is_palindrome(s, skips, left + 1, right)
-                    right_skip = is_palindrome(s, skips, left, right - 1)
+                    left_skip = is_palindrome(skips, left + 1, right)
+                    right_skip = is_palindrome(skips, left, right - 1)
                     return left_skip or right_skip
             return True
         
-        return is_palindrome(s, 0, 0, len(s) - 1)
+        return is_palindrome(0, 0, len(s) - 1)
     
         
 """        test driven way to write as minimum code as possible to make the test cases 'a', 'aa', 'aaa', 'aba', 'abba' work
