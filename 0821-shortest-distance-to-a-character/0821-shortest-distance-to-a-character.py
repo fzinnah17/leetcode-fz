fec31d172ed77dce2ebb64c7ee5ac648a5ac6ad2
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-21 21:23:23 -0400

    Time: 357 ms (5.04%), Space: 16.6 MB (94.21%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -1,11 +1,11 @@
 class Solution:
-  def shortestToChar(self, s, c):
-    ans = []
-    for i in range(len(s)):
-      min_distance = float('inf')  # Set initial distance to positive infinity
-      for j in range(len(s)):
-        if s[j] == c:
-          closest = abs(i - j)
-          min_distance = min(min_distance, closest)
-      ans.append(min_distance)  # Append the minimum distance for current index
-    return ans
+    def shortestToChar(self, s, c):
+        ans = []
+        for i in range(len(s)):
+            min_distance = float('inf')  # Set initial distance to positive infinity
+            for j in range(len(s)):
+                if s[j] == c:
+                    closest = abs(i - j)
+                    min_distance = min(min_distance, closest)
+            ans.append(min_distance)  # Append the minimum distance for current index
+        return ans
