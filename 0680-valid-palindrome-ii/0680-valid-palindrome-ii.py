5c401618dfd450d88aa87e363741d60cbad324bf
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-03-21 20:45:01 -0400

    Time: 98 ms (64.84%), Space: 16.9 MB (93.10%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,21 +1,32 @@
 class Solution:
+    
+    
     def validPalindrome(self, s: str) -> bool:
-        def check_palindrome(s, i, j):
-            while i < j:
-                if s[i] != s[j]:
-                    return False
-                i += 1
-                j -= 1
-            
-            return True
-
-        i = 0
-        j = len(s) - 1
-        while i < j:
-            # Found a mismatched pair - try both deletions
-            if s[i] != s[j]:
-                return check_palindrome(s, i, j - 1) or check_palindrome(s, i + 1, j)
-            i += 1
-            j -= 1
         
-        return True
+        def is_palindrome(s, skips, left, right):
+            if skips > 1:
+                return False
+
+            while left < right:
+                if s[left] == s[right]:
+                    left += 1
+                    right -= 1
+                else:
+                    skips += 1
+                    left_skip = is_palindrome(s, skips, left + 1, right)
+                    right_skip = is_palindrome(s, skips, left, right - 1)
+                    return left_skip or right_skip
+            return True
+        
+        return is_palindrome(s, 0, 0, len(s) - 1)
+    
+        
+"""        test driven way to write as minimum code as possible to make the test cases 'a', 'aa', 'aaa', 'aba', 'abba' work
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            if s[l] == s[r]:
+                l += 1
+                r -= 1
+        return True"""
+        
